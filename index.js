f84a1202126e4b117200065e648f7ac463d8c0e3
yaskevich <alyaxey.yaskevich@gmail.com>
2020-06-05 23:01:12 +0300

    single-token mode

--- index.js
+++ index.js
@@ -25,9 +25,10 @@ app.get('/',function(req,res){
 
 app.get('/api/texts', async (req, res, next) => {
   try {    
-    // const posts = await db.all('select f.id, f.title, count(l.id) as qty from   catspaw_folders as f left  join  catspaw_links as l  on f.id = l.fid group by f.id'); // <=
-	db.all("select strings.p, strings.form as v, strings.seq, strings.token_id as tid, tokens.token as t, tokens.meta as cl from strings left  join tokens on strings.token_id = tokens.id", function(err, rows) {
-		res.send(rows);
+    // tokens.meta as cl
+	db.all("select strings.id as sid, strings.p, strings.form as v, strings.s, strings.token_id as tid, strings.repr, tokens.token as utoken, strings.unit_id as uid, pos as cl, feats, lemma from strings left  join tokens on strings.token_id = tokens.id  left  join units on strings.unit_id = units.id", function(err, corpus) {
+        // console.log(corpus[0]);
+		res.send(corpus);
 	});
     
 	
@@ -38,17 +39,128 @@ app.get('/api/texts', async (req, res, next) => {
 
 app.post('/api/tokens', function(req, res){
 	var id = req.body.id;
+    var cls = req.body.cls;
+    var mode = +req.body.mode;
+    var uid = +req.body.uid;
+    var sid = +req.body.sid;
+	console.log(`Strings ID ${sid} Token ID ${id}, class ${cls}, single-mode ${mode}, unit ${uid}`); 
+    // db.run("UPDATE units SET pos = ? WHERE id = ?", [cls, id], function(err, row){
+        
+    db.all("SELECT id, pos from units where token_id = ?",[id], (err, units) => {
+        // console.log("units", units);
+        // let unit_db_id = uid;
+        
+        db.run("UPDATE tokens SET meta = ? WHERE id = ?", [cls, id], function(err, row){
+            if (err){
+                console.err(err);
+                res.status(500);
+            }
+            else {
+                if (mode) {
+                    let newuid = 0;
+                    for (let i = 0; i<units.length; i++){
+                        if(units[i]["pos"]==cls){
+                            newuid = units[i]["id"]
+                        }
+                    }
+                    
+                    if(newuid) {
+                        console.log("DB:",newuid);
+                    }
+                    
+                    let sql = newuid ? "SELECT * from units where pos = ? AND token_id = ?":  "INSERT INTO units (pos, token_id) VALUES (?, ?)";
+                    console.log(sql);
+                    db.run(sql, [cls, id], function(err, row){
+                        if (!newuid) {
+                            newuid  = this.lastID;
+                            console.log("last ID", this.lastID);
+                        }
+                        console.log("string", sid, newuid);
+                        if (sid){
+                            db.run("UPDATE strings SET unit_id = ? WHERE id = ?", [newuid, sid], function(err, row){
+                                res.json({"id": newuid, "pos": cls, "sid": sid});   
+                                         
+                            });
+                        } else {
+                            res.status(500);
+                            res.end();
+                        }
+                                             
+                    });
+                }
+                else {
+                    let sql = uid? 'UPDATE units SET pos = ? where id = ?' : "INSERT INTO units (pos, token_id) VALUES (?, ?)";
+                    
+                    db.run(sql, [cls, uid], function(err, row){
+                        // console.log(sql);
+                        if (!uid) {
+                            uid  = this.lastID;
+                            console.log("last ID", this.lastID);
+                        }
+                        // console.log("set", uid, id);
+                        db.run("UPDATE strings SET unit_id = ? WHERE token_id = ?", [uid, id], function(err, row){
+                            // db.get("SELECT COUNT(*) as res from tokens where meta is null or meta = ''", (err, row) => {
+                            db.all("SELECT token from tokens where meta is null or meta = ''", (err, row) => {
+                            // process the row here 
+                            console.log(row);
+                            // const tagged = Math.round(100 - +row["res"]/(11084/100), 1);
+                                // console.log(`${tagged} % [${row["res"]}]`);
+                            });
+                            // res.status(202);
+                            res.json({"id": uid, "pos": cls});                        
+                        });
+                        
+                    });
+                        
+                }        
+                // if (uid) {
+                
+                // if (units.length < 2) {
+                    // if (units.length){
+                        // unit_db_id  = units[0]["id"];
+                    // } else {
+                        
+                    // }
+                    
+                    // if (mode) {
+                        // console.log("SERVER: single mode!");
+                    // } else {
+                    // console.log("set", unit_db_id);
+                    // db.run("UPDATE strings SET unit_id = ? WHERE token_id = ?", [unit_db_id, id], function(err, row){
+                        
+                    // });
+
+
+                    // }
+                // } else {
+                    // console.log("two variants! not processed!")
+                // }              
+            
+                    
+                    
+
+            }
+            // res.end();
+        });        
+        
+
+    });            
+        
+
+	// res.status(500)
+	// res.send('error', { error: err })
+	// res.send()
+});
+
+function reverse(str){
+  return [...str].reverse().join('');
+}
+
+app.get('/api/data', function(req, res){
+	var id = req.body.id;
     var props = req.body.props;
-	console.log(id, props); 
-    db.run("UPDATE tokens SET meta = ? WHERE id = ?", [props, id], function(err, row){
-        if (err){
-            console.err(err);
-            res.status(500);
-        }
-        else {
-            res.status(202);
-        }
-        res.end();
+    db.all("SELECT * from  tokens where meta is null or meta = ''", (err, data) => {
+        res.json(data)
     });
 	// res.status(500)
 	// res.send('error', { error: err })
--- public/script.js
+++ public/script.js
@@ -1,15 +1,139 @@
 $(function() {   // Handler for .ready() called.
+$("title").text("Elemental – Smart Annotation tool");
+$(".single-mode").prop('checked', true); 
+
+// $(".modus").html($(".single-mode").prop('checked')?"single": "multi");
+    
+    function procMode() {
+        var mode = $('input[name="mode"]:not(:checked)').val();
+        // console.log("dis", mode);
+        $('.'+mode).addClass("d-none");
+        $('.'+$('input[name="mode"]:checked').val()).removeClass("d-none");
+    }
+    procMode();
+
+
+ // Generate dom elements
+const boxes = [
+    // ['.box-wrap.boxes.red', 252],
+    // ['.box-wrap.boxes.blue', 42],
+    ['.box-wrap.boxes.green', 4]
+];
+
+// for (const [sel, items] of boxes) {
+    // const container = document.querySelector(sel);
+
+    // for (let i = 0; i < items; i++) {
+        // console.log("11");
+        // container.appendChild(
+            // document.createElement('div')
+        // );
+    // }
+// }
+
+function reverse(str){
+  return str.split("").reverse().join("");
+}
+
+$.getJSON( "/api/data", function( data ) {
+     // { id: 2933, token: "i", meta: "", qty: 0, lemmas: 1 }
+    
+        // let dd = [];
+    // for (let x in data) {
+        // // console.log(data[x]["token"]);
+        // dd.push(data[x]["token"]);
+    // }
+    
+    data = data.sort(function(a, b){
+        if(reverse(a.token) < reverse(b.token)) { return -1; }
+        if(reverse(a.token) > reverse(b.token)) { return 1; }
+        return 0;
+    });
+    // data = data.sort(function(a, b){
+        // if(a.token < b.token) { return -1; }
+        // if(a.token > b.token) { return 1; }
+        // return 0;
+    // });
+    
+    $.each( data, function( key, val ) {
+        $('.box-wrap.boxes.green').append('<div class="row" data-id="'+val["id"]+'" data-token="'+val["token"]+'">'+ val["token"]+'</div>')
+    });
+});
+
+
+// Initialize selectionjs
+const selection = Selection.create({
+
+    // Class for the selection-area
+    class: 'selection',
+
+    // All elements in this container can be selected
+    selectables: ['.box-wrap > div'],
+
+    // The container is also the boundary in this case
+    boundaries: ['.box-wrap']
+}).on('start', ({inst, selected, oe}) => {
+
+    // Remove class if the user isn't pressing the control key or ⌘ key
+    if (!oe.ctrlKey && !oe.metaKey) {
+
+        // Unselect all elements
+        for (const el of selected) {
+            el.classList.remove('selected');
+            inst.removeFromSelection(el);
+        }
+
+        // Clear previous selection
+        inst.clearSelection();
+    }
+
+}).on('move', ({changed: {removed, added}}) => {
+
+    // Add a custom class to the elements that where selected.
+    for (const el of added) {
+        el.classList.add('selected');
+    }
+
+    // Remove the class from elements that where removed
+    // since the last selection
+    for (const el of removed) {
+        el.classList.remove('selected');
+    }
+
+}).on('stop', ({inst}) => {
+    inst.keepSelection();
+});
+
+
+
+
+
+var quickPos = '';
 
 var basePos = {
 	'vb': ["дзеяслоў", 'd'],
+	'vi': ["інфінітыў", ''],
+	'vg': ["дзеепрыслоўе", ''],
 	'nn': ["назоўнік", 'f'],
-	'nm': ["лічэбнік", 'x'],
+	'np': ["імя ўласнае", 'p'],
+	'nb': ["не беларускае", 'b'],
+	'nw': ["не слова", ''],
+	'part': ["часціца", ''],
+	'nm': ["лічэбнік", 'xx'],
 	'aj': ["прыметнік", 's'],
+    'va': ["дзеепрыметнік", ''],
 	'av': ["прыслоўе", 'a'],
 	'pn': ["займеннік", 'z'],
 	'pp': ["прыназоўнік", 'v'],
 	'cj': ["злучнік", 'c'],
-	'sm': ["іншае", 'q']
+	'intj': ["выклічнік", 'c'],
+	'det': ["вызначальнік", ''],
+	'aux': ["дапаможнік", ''],
+	'prad': ["займ. прысл", ''],
+	// 'dm': ["дыс. маркер", ''],
+	'mod': ["мадыфікатар", ''],
+	'ip': ["!?..", ''],
+	// 'sm': ["іншае", 'q']
 };
 // var hotKeys = {'d':'vb', 's': 'aj', 'z': 'pn', 'c':'cj',  'a':'av', 'x':'nm', 'f':'nn'}
 // var kiz = basePos.map((x) => ([x[2]]:x[0]));
@@ -26,14 +150,18 @@ var hotKeys = Object.entries(basePos).map(x=> ({[x[1][1]]:x[0]})).reduce(functio
 }, {});
 
 
-var specClasses;
-$.getJSON( "classes.json", function( data ) {
-	specClasses  = data;
-	// console.log(data);
-});
+// var specClasses;
+// $.getJSON( "classes.json", function( data ) {
+	// specClasses  = data;
+	// // console.log(data);
+// });
 
-$('.magicPos').prepend(Object.entries(basePos).map(x => 		
-	'<div style="margin-bottom:3px;"><button class="btn mgclick" data-pos="'+ x[0] +'">'+x[1][0]+'</b></div>'
+// $('.magicPos').prepend(Object.entries(basePos).map(x => 		
+	// '<div style="margin-bottom:3px;"><button class="btn mgclick" data-pos="'+ x[0] +'">'+x[1][0]+'</b></div>'
+// ).join(''));
+
+$('.toolbar').append(Object.entries(basePos).map(x => 		
+	'<button class="btn mgclick '+x[0]+'" data-pos="'+ x[0] +'">'+x[1][0]+'</b>'
 ).join(''));
 
 function makeRadio(group, val, title){
@@ -46,31 +174,62 @@ function makeRadio(group, val, title){
 var boardPos = Object.entries(basePos).map(x=> makeRadio('genpos', x[0], x[1][0])).join('');
 $('.board').prepend(boardPos);
 
-
-function setClassAndClose(wClass){
+var classes = Object.keys(basePos).join(" ");
+                               
+function setWordClass(wClass){
 	if (wClass){
 		var sel  = $(".processed");
 		// sel.removeClass('vb aj pn cj av nm nn').addClass(wClass.toLowerCase());
 		var tid  = sel.data('tid');
-		$('.editfield').children('a').each(function (i) {
-			if ($(this).data('tid') === tid){
-				$(this).removeClass('vb aj pn cj av nm nn').addClass(wClass);
-				// console.log(i);
-			}			
-		});
-		$.post( "/api/tokens", { "id": tid, "props": wClass}, function( data ) {
+		var uid  = sel.data('unit');
+		var sid  = sel.data('id');
+        
+        var singleMode = $('.single-mode').prop('checked');
+        
+        console.log("setting class", wClass);
+        
+
+        
+        
+		$.post( "/api/tokens", { "id": tid, "mode": singleMode|0, "sid": sid, "uid": uid|0, "cls": wClass}, function( data ) {
 			// console.log( "Data Loaded: " + data );
-			console.log( "token was successfully changed");
-			
+			console.log( "token was successfully changed", data);
+            
+            if(singleMode){ // tag only this token!
+                console.log("only single unit!");
+                sel.removeClass(classes).addClass(wClass+ " tagged");
+            } else { // tag every token of the same shape
+                console.log("for every repr of the token", tid, data.id);
+                $('.editfield').children('a').each(function (i) {
+                    if ($(this).data('tid') === tid){
+                        $(this).data("unit", data.id);
+                        $(this).removeClass(classes).addClass(wClass+ " tagged");
+                    }			
+                });
+            }
+            sel.removeClass("processed");            
+            
 
 		}).fail(function(response) {
 			// console.log('Error: ' + response.responseText);
-			console.log(response.status);
+			console.log("fail", response.status);
 		});
 	}
-	$('#modal').iziModal('close');
+	
 }
 	
+    
+function processClassChange(evt){
+   if ( evt.which == 13 ) {
+     evt.preventDefault();
+  }
+  console.log( evt.key );
+
+  if (hotKeys.hasOwnProperty(evt.key)){
+	  setWordClass(hotKeys[evt.key]);
+  } 
+}   
+ 
 $( "#modal" ).keypress(function( event ) {
   if ( event.which == 13 ) {
      event.preventDefault();
@@ -78,31 +237,65 @@ $( "#modal" ).keypress(function( event ) {
   console.log( event.key );
 
   if (hotKeys.hasOwnProperty(event.key)){
-	  setClassAndClose(hotKeys[event.key]);
+	  setWordClass(hotKeys[event.key]);
+      $('#modal').iziModal('close');
   }
 });
  
+ var tokens = {};
 
 $.getJSON( "/api/texts", function( data ) {
   var items = [];
   let previous = 0;
+  
+  var ok = true;
+  var curSent = 0;
+  var sentItems = [];
   $.each( data, function( key, val ) {
 	  // console.log(val);
 	  // items.push( "<div>"+val.map(x=> x.map(z=> (console.log(z), JSON.stringify(z))))+"</div>" );
 	  // words =  words.map(x => '<a class="token '+(x.trim().match(/[\s\n\r.,:\"\+!?-]+/)?'btn punct':'btn')+'">'+x+'</a>');
 	  if (previous !== val.p){
-		  items.push( '<div></div>' );
+		  sentItems.push( '<div></div>' );
 		  previous = val.p;
 	  }
-		if (val.cl !== 'ip' || val.t=='-') {
-			items.push( '<a class="token btn btn-sm '+ val.cl + ' "data-token="'+val.t+'" data-tid="'+val.tid+'">' + val.v+'</a>' );
+      // console.log(curSent, val.s);
+      
+        if ((val.s !== curSent)){
+            // console.log(curSent, ok);
+            
+            // if(!ok){
+            if(true) {
+            // if(!ok){
+                // console.log(curSent, "push");
+                $.merge( items, sentItems);
+                ok = true;
+            }
+            sentItems = [];
+            curSent = val.s;
+        }       
+      
+		// if (val.cl !== 'ip' || val.t=='-' || val.t=='–' ) {
+		if (val.cl !== 'ip' || val.t == '-' || val.t == '–'|| val.t == '—') {
+            sentItems.push( '<a class="'+ (val.cl? val.cl + ' tagged':'') + ' token btn btn-sm"' + ' data-token="'+val.utoken+'" data-unit="'+(val.uid||'') + '" data-id="'+val.sid +'"  data-tid="'+val.tid+'">' + val.v+'</a>' );
+            if (!val.cl){
+                ok = false;
+            }
+            
+            if (tokens.hasOwnProperty(val.utoken)){
+                tokens[val.utoken] +=1;
+            } else {
+                tokens[val.utoken] =1;
+            }
 		}
 	
-	if (val.p === 50) {
-		return false;
-	}
+	// if (val.s === 50) { return false; }
   });
- 
+             if(!ok){
+                // console.log(curSent, "push");
+                $.merge( items, sentItems);
+                ok = true;
+            }
  $('.editfield').append(items);
   // $( "<ul/>", {
     // "class": "my-new-list",
@@ -110,6 +303,11 @@ $.getJSON( "/api/texts", function( data ) {
   // }).appendTo( "body" );
 });
 
+
+$('input[name="mode"]').click(function() {
+    // console.log("bebe");
+    procMode();
+  });
 	
 $("#modal").iziModal({
     // history: true,
@@ -145,28 +343,82 @@ $("#modal").iziModal({
 
 $('body').on('click', 'a.token', function(e) {
 			var txt = $(e.target).data("token");
-			$(e.target).toggleClass("processed");
-			$('#modal').iziModal('setTitle', txt);
-			$('#modal').iziModal('open');			 
-			console.log("open modal");
-     });	
-	 $('.pos2').on('click',function(e){
-		 var curClass = $('input[name=genpos]:checked').val();
-		 var subClasses = [];
-		 var boardClasses;
-		 $.each(specClasses, function (i, obj) {
-			 // console.log(obj);
-			 if (obj.pos === curClass){
-					subClasses.push(obj);					
-			 }			 
-		 });
-		 console.log(subClasses);
-		 if (subClasses.length>1){
-			 var boardPos = subClasses.map(x=> makeRadio('specclass', x['class'], x['desc']+' <i>'+x['ex']+'</i>')).join('');
-			$('.board').html(boardPos);
-		 }
-		// setClassAndClose($('input[name=genpos]:checked').val());
+            var tid  = $(e.target).data('tid');
+            $('.processed').removeClass('processed');
+            $(e.target).addClass("processed");
+            $('.inform').html(txt+" "+tokens[txt]);
+            // console.log($(e.target).data("unit"));
+            
+            
+            
+            if (quickPos) {
+                setWordClass(quickPos);
+            } else {
+                $('.editfield').children('a').each(function (i) {
+                    if ($(this).data('tid') === tid){
+                        $(this).addClass("processed");
+                    }			
+                });
+            }
+            
+			// $('#modal').iziModal('setTitle', txt);
+			// $('#modal').iziModal('open');			 
+			// console.log("open modal");
+}).keypress(function( event ) {
+  // processClassChange(event);
+}).on('click', '.mgclick', function(e) {
+    var newpos = $(this).data("pos");
+    // console.log("mode!", newpos);
+    var mode = $('input[name="mode"]:checked').val();
+    console.log("GLOBAL", mode);
+    $('.processed').removeClass('processed');
+    
+    if (mode === "moz") {
+        if (quickPos && quickPos === newpos){
+            quickPos = '';
+            $("title").text("Elemental – Smart Annotation tool")
+            console.log("disable quick mode");
+        } else {
+            $("title").text(basePos[newpos][0]);
+            quickPos = newpos;
+            console.log("QUICK", newpos);
+        }
+    } else {
+        
+        // non-mozaic - list mode
+        $('.row.selected').each(function(){
+            var that = $(this);            
+            console.log(that.data("id"), that.data("token"), "→", newpos);
+            $.post( "/api/tokens", { "id": that.data("id"), "props": newpos}, function( data ) {
+                // console.log( "Data Loaded: " + data );
+                console.log( "token was successfully changed");
+                that.remove();
+            }).fail(function(response) {
+                // console.log('Error: ' + response.responseText);
+                console.log(response.status);
+            });            
+        });        
+    }
+})
+
+;
+ $('.pos2').on('click',function(e){
+     var curClass = $('input[name=genpos]:checked').val();
+     var subClasses = [];
+     var boardClasses;
+     $.each(specClasses, function (i, obj) {
+         // console.log(obj);
+         if (obj.pos === curClass){
+                subClasses.push(obj);					
+         }			 
      });
+     console.log(subClasses);
+     if (subClasses.length>1){
+         var boardPos = subClasses.map(x=> makeRadio('specclass', x['class'], x['desc']+' <i>'+x['ex']+'</i>')).join('');
+        $('.board').html(boardPos);
+     }
+    // setClassAndClose($('input[name=genpos]:checked').val());
+ });
 	 
 	// let stop = /(?=[\s\n\r.,:\"\+!?-]+)/;
 	// var words = alltext.split(stop);
