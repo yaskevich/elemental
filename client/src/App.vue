ee75c5456427699022d08ce4504a2895bf0a1431
yaskevich <alyaxey.yaskevich@gmail.com>
2021-11-16 12:19:40 +0300

    render router-view and navigation with router-links in App [client]

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,14 +2,32 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import router from './router'
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <!-- <div id="main" v-if="dataReady"> -->
+  <div id="main">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/comments">Comments</router-link> |
+      <!-- <span v-if="loggedIn">
+        <router-link to="/profile">Моё</router-link> |
+        <a href ="#" @click="doLogOut">Выйти</a>
+      </span> -->
+      <!-- <span v-else>
+        <router-link to="/login">Войти</router-link>
+      </span> -->
+    </div>
+    <router-view/>
+</div>
+<!-- <div v-else>
+    загрузка...
+</div> -->
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,5 +35,17 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
