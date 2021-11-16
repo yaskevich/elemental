dded7628cc336d259f9fcb8f7ed823a44947b571
yaskevich <alyaxey.yaskevich@gmail.com>
2021-11-16 12:17:41 +0300

    add Comments component [client]

---
+++ client/src/components/Comments.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>List of comments</h1>
+
+  <div v-for="(value, key) in fruits" :key="key">{{value}}</div>
+
+</template>
+
+<script setup lang="ts">
+
+// import { ref } from 'vue'
+
+// defineProps<{ msg: string }>()
+
+// const count = ref(0)
+const fruits: string[] = ['Apple', 'Orange', 'Banana'];
+</script>
+
+<style scoped>
+
+</style>
