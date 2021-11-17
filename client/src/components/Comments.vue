96c2b5197ec357f04cb3b2496b7cfb3a1aa26f9c
yaskevich <alyaxey.yaskevich@gmail.com>
2021-11-17 11:47:39 +0300

    get test data from server in Comments [client]

--- client/src/components/Comments.vue
+++ client/src/components/Comments.vue
@@ -1,16 +1,24 @@
 <template>
   <h1>List of comments</h1>
 
-  <div v-for="(value, key) in fruits" :key="key">{{value}}</div>
+  <div v-for="(value, key) in fruits" :key="key" style="padding:.5rem;">
+    <router-link :to="'/comment/'+key" style="text-decoration: none;">
+      {{value}}
+    </router-link>
+  </div>
 
 </template>
 
 <script setup lang="ts">
-
+import store from '../store'
+import { ref, reactive, onBeforeMount } from 'vue'
 // import { ref } from 'vue'
 
 // defineProps<{ msg: string }>()
-
+onBeforeMount(async () => {
+  const data = await store.get('test');
+  console.log("data from server", data);
+});
 // const count = ref(0)
 const fruits: string[] = ['Apple', 'Orange', 'Banana'];
 </script>
