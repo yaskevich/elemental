2017fbe89de1340dc59cf307721c203a815ad0a9
yaskevich <alyaxey.yaskevich@gmail.com>
2021-11-17 16:47:21 +0300

    rename db -> sqlitedb to differ with db as PostgreSQL in index.js [server]

--- server/index.js
+++ server/index.js
@@ -9,13 +9,14 @@ import dotenv from 'dotenv';
 import sqlite3 from 'sqlite3';
 import bodyParser from 'body-parser';
 import nlp from './nlp.js';
+import db from './db.js';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 
 (async () => {
-const db = new sqlite3.Database(process.env.SQLITE);
+const sqlitedb = new sqlite3.Database(process.env.SQLITE);
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -39,7 +40,7 @@ app.get('/',function(req,res){
 app.get('/api/texts', async (req, res, next) => {
   try {
     // tokens.meta as cl
-	db.all("select strings.id as sid, strings.p, strings.form as v, strings.s, strings.token_id as tid, strings.repr, tokens.token as utoken, strings.unit_id as uid, pos as cl, feats, lemma from strings left  join tokens on strings.token_id = tokens.id  left  join units on strings.unit_id = units.id", function(err, corpus) {
+	sqlitedb.all("select strings.id as sid, strings.p, strings.form as v, strings.s, strings.token_id as tid, strings.repr, tokens.token as utoken, strings.unit_id as uid, pos as cl, feats, lemma from strings left  join tokens on strings.token_id = tokens.id  left  join units on strings.unit_id = units.id", function(err, corpus) {
         // console.log(corpus[0]);
 		res.send(corpus);
 	});
@@ -51,7 +52,7 @@ app.get('/api/texts', async (req, res, next) => {
 
 app.get('/conll', function (req, res) {
 	try {
-		db.all("select strings.id as sid, strings.p, strings.form as v, strings.s, strings.token_id as tid, strings.repr, tokens.token as utoken, strings.unit_id as uid, pos as cl, feats, lemma from strings left  join tokens on strings.token_id = tokens.id  left  join units on strings.unit_id = units.id", function(err, corpus) {
+		sqlitedb.all("select strings.id as sid, strings.p, strings.form as v, strings.s, strings.token_id as tid, strings.repr, tokens.token as utoken, strings.unit_id as uid, pos as cl, feats, lemma from strings left  join tokens on strings.token_id = tokens.id  left  join units on strings.unit_id = units.id", function(err, corpus) {
 			const conll = nlp.convertToConll(corpus);
 			res.send(conll);
 	});
@@ -67,12 +68,12 @@ app.post('/api/tokens', function(req, res){
     var uid = +req.body.uid;
     var sid = +req.body.sid;
 	console.log(`Strings ID ${sid} Token ID ${id}, class ${cls}, single-mode ${mode}, unit ${uid}`);
-    // db.run("UPDATE units SET pos = ? WHERE id = ?", [cls, id], function(err, row){
+    // sqlitedb.run("UPDATE units SET pos = ? WHERE id = ?", [cls, id], function(err, row){
 
-    db.all("SELECT id, pos from units where token_id = ?",[id], (err, units) => {
+    sqlitedb.all("SELECT id, pos from units where token_id = ?",[id], (err, units) => {
         // console.log("units", units);
         // let unit_db_id = uid;
-        db.run("UPDATE tokens SET meta = ? WHERE id = ?", [cls, id], function(err, row){
+        sqlitedb.run("UPDATE tokens SET meta = ? WHERE id = ?", [cls, id], function(err, row){
             if (err){
                 console.err(err);
                 res.status(500);
@@ -92,14 +93,14 @@ app.post('/api/tokens', function(req, res){
 
                     let sql = newuid ? "SELECT * from units where pos = ? AND token_id = ?":  "INSERT INTO units (pos, token_id) VALUES (?, ?)";
                     console.log(sql);
-                    db.run(sql, [cls, id], function(err, row){
+                    sqlitedb.run(sql, [cls, id], function(err, row){
                         if (!newuid) {
                             newuid  = this.lastID;
                             console.log("last ID", this.lastID);
                         }
                         console.log("string", sid, newuid);
                         if (sid){
-                            db.run("UPDATE strings SET unit_id = ? WHERE id = ?", [newuid, sid], function(err, row){
+                            sqlitedb.run("UPDATE strings SET unit_id = ? WHERE id = ?", [newuid, sid], function(err, row){
                                 res.json({"id": newuid, "pos": cls, "sid": sid});
                             });
                         } else {
@@ -110,16 +111,16 @@ app.post('/api/tokens', function(req, res){
                 }
                 else {
                     let sql = uid? 'UPDATE units SET pos = ? where id = ?' : "INSERT INTO units (pos, token_id) VALUES (?, ?)";
-                    db.run(sql, [cls, uid], function(err, row){
+                    sqlitedb.run(sql, [cls, uid], function(err, row){
                         // console.log(sql);
                         if (!uid) {
                             uid  = this.lastID;
                             console.log("last ID", this.lastID);
                         }
                         // console.log("set", uid, id);
-                        db.run("UPDATE strings SET unit_id = ? WHERE token_id = ?", [uid, id], function(err, row){
-                            // db.get("SELECT COUNT(*) as res from tokens where meta is null or meta = ''", (err, row) => {
-                            db.all("SELECT token from tokens where meta is null or meta = ''", (err, row) => {
+                        sqlitedb.run("UPDATE strings SET unit_id = ? WHERE token_id = ?", [uid, id], function(err, row){
+                            // sqlitedb.get("SELECT COUNT(*) as res from tokens where meta is null or meta = ''", (err, row) => {
+                            sqlitedb.all("SELECT token from tokens where meta is null or meta = ''", (err, row) => {
                             // process the row here
                             console.log(row);
                             // const tagged = Math.round(100 - +row["res"]/(11084/100), 1);
@@ -140,7 +141,7 @@ app.post('/api/tokens', function(req, res){
                         // console.log("SERVER: single mode!");
                     // } else {
                     // console.log("set", unit_db_id);
-                    // db.run("UPDATE strings SET unit_id = ? WHERE token_id = ?", [unit_db_id, id], function(err, row){
+                    // sqlitedb.run("UPDATE strings SET unit_id = ? WHERE token_id = ?", [unit_db_id, id], function(err, row){
                     // });
                     // }
                 // } else {
@@ -155,7 +156,7 @@ app.post('/api/tokens', function(req, res){
 app.get('/api/data', function(req, res){
 	var id = req.body.id;
     var props = req.body.props;
-    db.all("SELECT * from  tokens where meta is null or meta = ''", (err, data) => {
+    sqlitedb.all("SELECT * from  tokens where meta is null or meta = ''", (err, data) => {
         res.json(data)
     });
 });
