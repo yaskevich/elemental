ea64d93f7a5fad842d8e363c0412214b1c1c1b06
yaskevich <alyaxey.yaskevich@gmail.com>
2021-11-18 13:15:55 +0300

    add processToken function stub to db [server]

--- server/db.js
+++ server/db.js
@@ -20,6 +20,7 @@ const { Pool } = pg;
 const pool = new Pool();
 
 
+
 export default {
   async getCorpusAsConll() {
     const sql = `select strings.id as sid, strings.p, strings.form as v, strings.s, strings.token_id as tid, strings.repr, tokens.token as utoken, strings.unit_id as uid, pos as cl from strings left  join tokens on strings.token_id = tokens.id  left  join units on strings.unit_id = units.id order by sid`;
@@ -54,4 +55,90 @@ export default {
     }
     return data;
   },
-}
+  async processToken(tokenId, wordClass, mode, unitId, sentenceId) {
+    // // sqlitedb.run("UPDATE units SET pos = ? WHERE id = ?", [cls, id], function(err, row){
+    // sqlitedb.all("SELECT id, pos from units where token_id = ?",[id], (err, units) => {
+        // console.log("units", units);
+        // let unit_db_id = uid;
+    //     sqlitedb.run("UPDATE tokens SET meta = ? WHERE id = ?", [cls, id], function(err, row){
+    //         if (err){
+    //             console.err(err);
+    //             res.status(500);
+    //         }
+    //         else {
+    //             if (mode) {
+    //                 let newuid = 0;
+    //                 for (let i = 0; i<units.length; i++){
+    //                     if(units[i]["pos"]==cls){
+    //                         newuid = units[i]["id"]
+    //                     }
+    //                 }
+    //
+    //                 if(newuid) {
+    //                     console.log("DB:",newuid);
+    //                 }
+    //
+    //                 let sql = newuid ? "SELECT * from units where pos = ? AND token_id = ?":  "INSERT INTO units (pos, token_id) VALUES (?, ?)";
+    //                 console.log(sql);
+    //                 sqlitedb.run(sql, [cls, id], function(err, row){
+    //                     if (!newuid) {
+    //                         newuid  = this.lastID;
+    //                         console.log("last ID", this.lastID);
+    //                     }
+    //                     console.log("string", sid, newuid);
+    //                     if (sid){
+    //                         sqlitedb.run("UPDATE strings SET unit_id = ? WHERE id = ?", [newuid, sid], function(err, row){
+    //                             res.json({"id": newuid, "pos": cls, "sid": sid});
+    //                         });
+    //                     } else {
+    //                         res.status(500);
+    //                         res.end();
+    //                     }
+    //                 });
+    //             }
+    //             else {
+    //                 let sql = uid? 'UPDATE units SET pos = ? where id = ?' : "INSERT INTO units (pos, token_id) VALUES (?, ?)";
+    //                 sqlitedb.run(sql, [cls, uid], function(err, row){
+    //                     // console.log(sql);
+    //                     if (!uid) {
+    //                         uid  = this.lastID;
+    //                         console.log("last ID", this.lastID);
+    //                     }
+    //                     // console.log("set", uid, id);
+    //                     sqlitedb.run("UPDATE strings SET unit_id = ? WHERE token_id = ?", [uid, id], function(err, row){
+    //                         // sqlitedb.get("SELECT COUNT(*) as res from tokens where meta is null or meta = ''", (err, row) => {
+    //                         sqlitedb.all("SELECT token from tokens where meta is null or meta = ''", (err, row) => {
+    //                         // process the row here
+    //                         console.log(row);
+    //                         // const tagged = Math.round(100 - +row["res"]/(11084/100), 1);
+    //                             // console.log(`${tagged} % [${row["res"]}]`);
+    //                         });
+    //                         // res.status(202);
+    //                         res.json({"id": uid, "pos": cls});
+    //                     });
+    //                 });
+    //             }
+    //                               // if (uid) {
+    //                               // if (units.length < 2) {
+    //                                   // if (units.length){
+    //                                       // unit_db_id  = units[0]["id"];
+    //                                   // } else {
+    //                                   // }
+    //                                   // if (mode) {
+    //                                       // console.log("SERVER: single mode!");
+    //                                   // } else {
+    //                                   // console.log("set", unit_db_id);
+    //                                   // sqlitedb.run("UPDATE strings SET unit_id = ? WHERE token_id = ?", [unit_db_id, id], function(err, row){
+    //                                   // });
+    //                                   // }
+    //                               // } else {
+    //                                   // console.log("two variants! not processed!")
+    //                               // }
+    //         }
+    //                             // res.end();
+    //     });
+    // });
+
+    return {tokenId, wordClass, mode, unitId, sentenceId};
+  },
+};
