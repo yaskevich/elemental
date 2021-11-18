6ef53476fdefc5fc6a29931385b07e86b181c829
yaskevich <alyaxey.yaskevich@gmail.com>
2021-11-18 11:29:01 +0300

    add functions getStrings and getUntagged in db [server]

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ const configLoaded = dotenv.config();
 // if it is not run under PM2 and dotenv config is not provided
 if (!process.env.NODE_ENV && configLoaded.error) {
   console.error(configLoaded.error.toString());
-	process.exit(1);
+  process.exit(1);
 }
 
 // import bcrypt from 'bcrypt';
@@ -20,18 +20,38 @@ const { Pool } = pg;
 const pool = new Pool();
 
 
- // const selectResult = await pool.query(`${selectables[table]} WHERE id = ${id}`);
-
- export default {
-   async getCorpusAsConll() {
-     const sql = `select strings.id as sid, strings.p, strings.form as v, strings.s, strings.token_id as tid, strings.repr, tokens.token as utoken, strings.unit_id as uid, pos as cl, feats, lemma from strings left  join tokens on strings.token_id = tokens.id  left  join units on strings.unit_id = units.id order by sid`;
-     let conll = [];
-     try {
+export default {
+  async getCorpusAsConll() {
+    const sql = `select strings.id as sid, strings.p, strings.form as v, strings.s, strings.token_id as tid, strings.repr, tokens.token as utoken, strings.unit_id as uid, pos as cl from strings left  join tokens on strings.token_id = tokens.id  left  join units on strings.unit_id = units.id order by sid`;
+    let conll = [];
+    try {
       const result = await pool.query(sql);
       conll = result?.rows;
-     } catch (err) {
-       console.error(err);
-     }
-     return conll;
-   },
- }
+    } catch (err) {
+      console.error(err);
+    }
+    return conll;
+  },
+  async getStrings() {
+    const sql = `select strings.id as sid, strings.p, strings.form as v, strings.s, strings.token_id as tid, strings.repr, tokens.token as utoken, strings.unit_id as uid, pos as cl from strings left  join tokens on strings.token_id = tokens.id  left  join units on strings.unit_id = units.id order by sid`;
+    let data = [];
+    try {
+      const result = await pool.query(sql);
+      data = result?.rows;
+    } catch (err) {
+      console.error(err);
+    }
+    return data;
+  },
+  async getUntagged() {
+    const sql = `SELECT * from  tokens where meta is null or meta = ''`;
+    let data = [];
+    try {
+      const result = await pool.query(sql);
+      data = result?.rows;
+    } catch (err) {
+      console.error(err);
+    }
+    return data;
+  },
+}
