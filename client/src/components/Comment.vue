bebd2a7e20318ab2c8564d20705d37bee51351fb
yaskevich <alyaxey.yaskevich@gmail.com>
2021-11-20 12:42:31 +0300

    render comment props from server in Comment [client]

--- client/src/components/Comment.vue
+++ client/src/components/Comment.vue
@@ -1,25 +1,64 @@
 <template>
-  <h1>Single comment</h1>
+
+  <h1>{{entry.title}}</h1>
   <div>
     <div style="max-width:300px;margin: 0 auto;">
-      <Tiptap/>
+      <n-switch style="padding-bottom:1rem;" v-model:value="entry.published">
+        <template #checked>Published</template>
+        <template #unchecked>Draft</template>
+      </n-switch>
+      <n-input v-model:value="entry.title" type="text" placeholder="Heading" />
+      <Tiptap ref="tiptapRef" />
     </div>
+    <n-button type="primary" @click="saveComment">Save</n-button>
   </div>
 
 </template>
 
 <script setup lang="ts">
-import store from '../store'
-import { ref, reactive, onBeforeMount } from 'vue'
-import Tiptap from './Tiptap.vue'
-// import { ref } from 'vue'
 
-// defineProps<{ msg: string }>()
-// onBeforeMount(async () => {
-//   const data = await store.get('test');
-//   console.log("data from server", data);
-// });
-// const count = ref(0)
+  import store from '../store';
+  import { ref, reactive, onBeforeMount, } from 'vue';
+  import Tiptap from './Tiptap.vue';
+  import { useRoute } from 'vue-router';
+  // defineProps<{ msg: string }>()
+
+  const vuerouter = useRoute();
+  const id = vuerouter.params.id;
+  const entry = reactive({});
+  const tiptapRef = ref(null);
+
+  // const json = ref({});
+  // let json = {};
+
+  onBeforeMount(async () => {
+    if (id) {
+      const data = await store.get(`comments/${id}`);
+      console.log('data from server', data);
+      if (data.length) {
+        console.log("data", data);
+        Object.assign(entry, data[0]);
+
+
+        const editorInstance = tiptapRef.value?.editor;
+        if (editorInstance) {
+           editorInstance.commands.setContent(data[0].content_json, false);
+        }
+      }
+    }
+  });
+
+  const saveComment = () => {
+    const editorInstance = tiptapRef.value?.editor;
+    if (editorInstance) {
+      entry.content_json = editorInstance.getJSON();
+      // console.log(JSON.stringify(entry.content_json));
+      entry.content_html = editorInstance.getHTML();
+      entry.content_text = editorInstance.getText();
+      console.log('Content', entry.content_text);
+    }
+  };
+
 </script>
 
 <style scoped>
