b9940b5df9e72dfbf8c4b4dfed7648dd004cc979
yaskevich <alyaxey.yaskevich@gmail.com>
2021-11-20 12:39:51 +0300

    update Tiptap options [client]

--- client/src/components/Tiptap.vue
+++ client/src/components/Tiptap.vue
@@ -1,47 +1,65 @@
 <template>
-  <editor-content :editor="editor" style="display: inline-block; text-align: left;min-width:300px;"/>
+
+  <editor-content :editor="editor" style="display: inline-block; text-align: left;min-width:300px;" />
+
 </template>
 
 <script>
-import { useEditor, EditorContent } from '@tiptap/vue-3'
-// import StarterKit from '@tiptap/starter-kit'
-import Document from '@tiptap/extension-document'
-import Paragraph from '@tiptap/extension-paragraph'
-import Text from '@tiptap/extension-text'
-import History from '@tiptap/extension-history'
-import TextStyle from '@tiptap/extension-text-style'
-import Color from '@tiptap/extension-color'
 
-export default {
-  components: {
-    EditorContent,
-  },
+  import { useEditor, EditorContent } from '@tiptap/vue-3';
+  // import StarterKit from '@tiptap/starter-kit'
+  import Document from '@tiptap/extension-document';
+  import Paragraph from '@tiptap/extension-paragraph';
+  import Text from '@tiptap/extension-text';
+  import History from '@tiptap/extension-history';
+  import TextStyle from '@tiptap/extension-text-style';
+  import Color from '@tiptap/extension-color';
+  import Placeholder from '@tiptap/extension-placeholder';
 
-  setup() {
-    const editor = useEditor({
-      content: '<p>Some editable text...</p>',
-      autofocus: true,
-      editable: true,
-      extensions: [
-        Document,
-        Paragraph,
-        Text,
-        History,
-        TextStyle,
-        Color,
-        // StarterKit,
-      ],
-    })
+  export default {
+    components: {
+      EditorContent,
+    },
 
-    return { editor }
-  },
-}
+    setup() {
+      const editor = useEditor({
+        content: '',
+        autofocus: 'end',
+        editable: true,
+        extensions: [
+          Document,
+          Paragraph,
+          Text,
+          History,
+          TextStyle,
+          Color,
+          Placeholder.configure({
+            placeholder: 'Start writing your comment...',
+          }),
+          // StarterKit,
+        ],
+      });
+
+      return { editor };
+    },
+  };
+
 </script>
 
 <style>
-.ProseMirror p {
-  margin: 1em 0;
-  background-color:lightyellow;
-  padding: 15px;
-}
+
+  .ProseMirror p {
+    margin: 1em 0;
+    background-color: lightyellow;
+    padding: 15px;
+  }
+
+  .ProseMirror p.is-editor-empty:first-child::before {
+    content: attr(data-placeholder);
+    float: left;
+    color: #adb5bd;
+    pointer-events: none;
+    height: 0;
+  }
+
 </style>
