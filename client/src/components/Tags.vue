7c8ddb356d4246e03ce9d187eb259b21563591f8
yaskevich <alyaxey.yaskevich@gmail.com>
2021-11-22 23:31:15 +0300

    handle editing/adding tags in Tags [client]

--- client/src/components/Tags.vue
+++ client/src/components/Tags.vue
@@ -2,13 +2,21 @@
 
   <h1>Tags</h1>
 
-  <n-button type="info" dashed @click="addTag">Create new tags +</n-button>
+  <n-space vertical>
+    <n-input style="min-width:250px;" autosize v-model:value="newTag.en" type="text" placeholder="English title" />
+    <n-input autosize style="min-width:250px;" v-model:value="newTag.ru" type="text" placeholder="Russian title" />
+    <n-button type="info" dashed @click="editTag(newTag)">Create new tag +</n-button>
+  </n-space>
   <n-divider style="width:300px;text-align: center; margin:auto;padding:1rem;" />
   <div v-if="!tags.length">
     <n-text type="error">There are no tags!</n-text>
   </div>
-  <div v-for="item in tags" :key="item.id" style="padding:.5rem;">
-    {{item.en}} | {{item.ru}}
+  <div v-for="item in tags" :key="item.id" style="padding:.5rem;margin: 0 auto;display: table;">
+    <n-input-group>
+      <n-input autosize v-model:value="item.en" placeholder="English title" />
+      <n-input autosize v-model:value="item.ru" placeholder="Russian title" />
+      <n-button type="primary" ghost @click="editTag(item)">Save</n-button>
+    </n-input-group>
   </div>
 
 </template>
@@ -18,6 +26,7 @@
   import store from '../store';
   import { ref, reactive, onBeforeMount } from 'vue';
 
+  const newTag = reactive({ en: '', ru: '' });
   const tags = reactive([]);
   onBeforeMount(async () => {
     const data = await store.get('tags');
@@ -25,8 +34,25 @@
     console.log('data from server', data);
   });
 
-  const addComment = () => {
-    console.log('create tag');
+  const editTag = async tag => {
+    console.log('edit tag', tag);
+    const params = {};
+    if (tag?.id) {
+      params.id = tag.id;
+    }
+    params.en = tag.en;
+    params.ru = tag.ru;
+    const result = await store.post('tag', params);
+    if (result?.data?.id) {
+        params.id = result.data.id;
+        if (!tag?.id) {
+          tags.push(params);
+          newTag.en = newTag.ru = '';
+        }
+    } else {
+        console.log('error', result);
+    }
+
   };
 
 </script>
