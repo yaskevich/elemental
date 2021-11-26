5e205aa86ecacca78a4ea9d8b148ed71b1595feb
yaskevich <alyaxey.yaskevich@gmail.com>
2021-11-26 19:48:17 +0300

    enable Naive UI components [client]

--- client/src/main.ts
+++ client/src/main.ts
@@ -21,10 +21,11 @@ import {
   NFormItem,
   NRadioGroup,
   NRadio,
+  NSelect,
 } from 'naive-ui'
 
 const naive = create({
-  components: [NButton, NText, NInput, NSwitch, NDivider, NAvatar, NIcon, NSpace, NInputGroup,NDynamicTags, NColorPicker, NMessageProvider, NForm, NFormItem, NRadioGroup, NRadio,]
+  components: [NButton, NText, NInput, NSwitch, NDivider, NAvatar, NIcon, NSpace, NInputGroup,NDynamicTags, NColorPicker, NMessageProvider, NForm, NFormItem, NRadioGroup, NRadio, NSelect, ]
 })
 
 createApp(App)
