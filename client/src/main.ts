cec91c7d25fdde9d91526e6ef95bc726f737c29e
yaskevich <alyaxey.yaskevich@gmail.com>
2021-11-30 23:37:27 +0300

    enable Naive UI components (NTag) [client]

--- client/src/main.ts
+++ client/src/main.ts
@@ -15,6 +15,7 @@ import {
   NSpace,
   NInputGroup,
   NDynamicTags,
+  NTag,
   NColorPicker,
   NMessageProvider,
   NForm,
@@ -26,7 +27,7 @@ import {
 } from 'naive-ui'
 
 const naive = create({
-  components: [NButton, NText, NInput, NSwitch, NDivider, NAvatar, NIcon, NSpace, NInputGroup,NDynamicTags, NColorPicker, NMessageProvider, NForm, NFormItem, NRadioGroup, NRadio, NSelect, NDropdown, ]
+  components: [NButton, NText, NInput, NSwitch, NDivider, NAvatar, NIcon, NSpace, NInputGroup,NDynamicTags, NColorPicker, NMessageProvider, NForm, NFormItem, NRadioGroup, NRadio, NSelect, NDropdown, NTag, ]
 })
 
 createApp(App)
