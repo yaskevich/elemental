9b22e5228e08b12ee4a416c1a1f0738feeee87eb
yaskevich <alyaxey.yaskevich@gmail.com>
2021-12-01 22:54:20 +0300

    disallow selecting again items already set in issues and tags in Comment [client]

--- client/src/components/Comment.vue
+++ client/src/components/Comment.vue
@@ -3,6 +3,11 @@
   <!-- <h1>{{entry.title}}</h1> -->
   <div>
     <div style="max-width:300px;margin: 0 auto;">
+      <div style="margin-bottom: 10px;">
+        <n-tag v-for="issue in entry.issues" :key="issue" closable @close="removeTag(issue)" :color="{ color: issuesKV[issue].color, textColor: 'white', }" style="mix-blend-mode: difference;">
+          {{issuesKV[issue]?.["ru"]}}
+        </n-tag>
+      </div>
       <div style="margin-bottom:.5rem;">
         <n-input v-model:value="entry.num_id" style="width:75px;" type="text" placeholder="ID"/>
         <n-switch style="padding-left:1rem;padding-bottom: 5px;" v-model:value="entry.published">
@@ -10,9 +15,22 @@
           <template #unchecked>Draft</template>
         </n-switch>
         <router-link :to="'/preview/'+entry.id" style="margin-left:5px;">Preview</router-link>
+        <n-dropdown trigger="hover" @select="addTag" :options="tagsList">
+          <n-button> + tag</n-button>
+        </n-dropdown>
+        <n-dropdown trigger="hover" @select="addIssue" :options="issuesList">
+          <n-button> + issue</n-button>
+        </n-dropdown>
+
       </div>
       <n-text type="warning" v-if="!entry.num_id">ID should not be empty!</n-text>
       <n-input v-model:value="entry.title" type="text" placeholder="Heading" class="maininput" />
+      <n-divider style="width:300px;text-align: center; margin:auto;padding:1rem;"/>
+      <n-tag v-for="tag in entry.tags" :key="tag" closable @close="removeTag(tag)" style="margin-right: 10px;" size="small">
+        {{tagsList.filter(x => x.key == tag)?.shift()?.["label"]}}
+      </n-tag>
+
+
       <n-text type="warning" v-if="!entry.title">Heading should not be empty!</n-text>
       <n-divider style="width:300px;text-align: center; margin:auto;padding:1rem;"/>
       <n-input v-model:value="entry.trans" type="text" placeholder="Translation" />
@@ -20,26 +38,6 @@
       <Tiptap ref="briefRef" editorclass="briefeditor" />
       <n-divider style="width:300px;text-align: center; margin:auto;padding:1rem;"/>
       <Tiptap ref="contentRef" editorclass="fulleditor"/>
-      <h4>Tags</h4>
-
-      <n-tag v-for="tag in entry.tags" :key="tag" closable @close="removeTag(tag)">
-        {{tagsList.filter(x => x.key == tag)?.shift()?.["label"]}}
-      </n-tag>
-
-      <n-dropdown trigger="hover" @select="addTag" :options="tagsList">
-        <n-button> Add a tag</n-button>
-      </n-dropdown>
-
-
-      <h4>Issues</h4>
-      <n-tag v-for="issue in entry.issues" :key="issue" closable @close="removeTag(issue)">
-        {{issuesList.filter(x => x.key == issue)?.shift()?.["label"]}}
-      </n-tag>
-
-      <n-dropdown trigger="hover" @select="addIssue" :options="issuesList">
-        <n-button> Add an issue</n-button>
-      </n-dropdown>
-
     </div>
     <n-divider style="width:300px;text-align: center; margin:auto;padding:1rem;"/>
     <n-button type="primary" @click="saveComment" v-if="entry.title && entry.num_id">Save</n-button>
@@ -51,7 +49,7 @@
 <script setup lang="ts">
 
   import store from '../store';
-  import { ref, reactive, onBeforeMount, } from 'vue';
+  import { ref, reactive, onBeforeMount, computed, } from 'vue';
   import Tiptap from './Tiptap.vue';
   import { useRoute } from 'vue-router';
   // defineProps<{ msg: string }>()
@@ -66,21 +64,18 @@
 
   const tagsList = reactive([]);
   const issuesList = reactive([]);
-  // const json = ref({});
-  // let json = {};
+
+  const issuesKV = reactive({});
 
   const removeTag = (id) => {
-        console.log(id);
         entry.tags = entry.tags.filter(x => x !== id);
   };
 
   const addTag = (id) => {
-        console.log(id);
         entry.tags.push(id);
   };
 
   const removeIssue = (id) => {
-        console.log(id);
         entry.issues = entry.issues.filter(x => x !== id);
   };
 
@@ -92,17 +87,20 @@
   onBeforeMount(async () => {
     if (id) {
       const tagData = await store.get('tags');
-      console.log("tags", tagData);
-      Object.assign(tagsList, tagData.map(x => ({label: x.ru, key: x.id, disabled: false})));
+      // console.log("tags", tagData);
+      Object.assign(tagsList, tagData.map(x => ({label: x.ru, key: x.id, disabled: computed(() => entry.tags?.includes (x.id))})));
 
       const issueData = await store.get('issues');
-      const issueListData = issueData.map(x => ({label: x.ru, key: x.id, disabled: false}));
+      const issueListData = issueData.map(x => ({label: x.ru, key: x.id, disabled: computed(() => entry.issues?.includes (x.id))}));
       Object.assign(issuesList, issueListData);
+
+      Object.assign(issuesKV, Object.fromEntries(issueData.map(x => [x.id, x])));
+      console.log(issuesKV);
 
       const data = await store.get(`comment/${id}`);
       console.log('data from server', data);
       if (data.length) {
-        console.log("data", data);
+        // console.log("data", data);
         Object.assign(entry, data[0]);
         entry.tags = [14];
 
