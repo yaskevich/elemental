221a6c5c15c37017a8729d08d2efbf3745c9b583
yaskevich <alyaxey.yaskevich@gmail.com>
2021-12-05 18:33:08 +0300

    enable dev mode Typescript check [client]

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), Checker({ typescript: true })],
   server: {
     port: 3090,
     proxy: {
