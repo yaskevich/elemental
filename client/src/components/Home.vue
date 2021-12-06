350a4587061c5fc1758a249fa37524f773e1e0b3
yaskevich <alyaxey.yaskevich@gmail.com>
2021-12-06 14:54:52 +0300

    render app info in NDescription in Home [client]

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -15,7 +15,9 @@ onBeforeMount(async () => {
 
 const goToText = (id: number) => {
   // console.log("go to text", id);
+  localStorage.setItem('text_id', String(id));
   router.push(`/comments/${id}`);
+  store.state.user.text_id = id;
 }
 
 const state = store.state;
@@ -24,10 +26,12 @@ const state = store.state;
 <template>
   <!-- <h1>{{ msg }}</h1> -->
   <h3>Projects</h3>
-
+  <div class="center-column">
+    <div class="left-column">
   <!-- <p>See <code>README.md</code> for more information.</p> -->
   <div v-for="(value, key) in data" :key="key" style="padding:.5rem;" :title="value.meta">
-    <n-button text @click="goToText(value.id)">
+     <!-- <n-button type="info">Info</n-button> -->
+    <n-button @click="goToText(value.id)" :type="value.id === store.state.user.text_id ? 'info': ''">
       {{value.author}}.&nbsp;{{value.title}}
       <!-- <n-text type="info">{{value.author}}</n-text>.&nbsp;
       «<n-text strong>{{value.title}}</n-text>» -->
@@ -35,10 +39,14 @@ const state = store.state;
   </div>
   <!-- <button type="button" @click="count++">count is: {{ count }}</button> -->
   <n-divider style="width:300px;text-align: center; margin:auto;padding:1rem;" />
+  <n-descriptions label-placement="top" bordered :column="3" size="small">
+    <n-descriptions-item label="Client">{{store.version}}</n-descriptions-item>
+    <n-descriptions-item label="Server">{{state.user.server}}</n-descriptions-item>
+    <n-descriptions-item label="Commit"><a type="primary" :href="'https://github.com/yaskevich/persona/commit/' + state.user.commit" target="_blank">{{state.user.commit}}</a></n-descriptions-item>
 
-  <div>Client version {{store.version}}</div>
-  <div>Server version {{state.user.server}}</div>
-  <div>Commit <a type="primary" :href="'https://github.com/yaskevich/persona/commit/' + state.user.commit" target="_blank">{{state.user.commit}}</a></div>
+  </n-descriptions>
+</div>
+</div>
 </template>
 
 <style scoped>
