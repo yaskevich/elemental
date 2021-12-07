efbbaa2f48cca40f632450ab1d28b9243fbb78d3
yaskevich <alyaxey.yaskevich@gmail.com>
2021-12-07 18:16:28 +0300

    render commit link via NButton in Home [client]

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,68 +1,80 @@
 <script setup lang="ts">
-import { ref, reactive, onBeforeMount } from 'vue'
-import store from '../store'
-import router from '../router';
 
-// defineProps<{ msg: string }>()
+  import { ref, reactive, onBeforeMount } from 'vue';
+  import store from '../store';
+  import router from '../router';
 
-// const msg = "Smart Annotation Tool"
+  // defineProps<{ msg: string }>()
 
-const data = ref();
+  // const msg = "Smart Annotation Tool"
 
-onBeforeMount(async () => {
-  data.value = await store.get('texts');
-});
+  const data = ref();
 
-const goToText = (id: number) => {
-  // console.log("go to text", id);
-  localStorage.setItem('text_id', String(id));
-  router.push(`/comments/${id}`);
-  store.state.user.text_id = id;
-}
+  onBeforeMount(async () => {
+    data.value = await store.get('texts');
+  });
 
-const state = store.state;
+  const goToText = (id: number) => {
+    // console.log("go to text", id);
+    localStorage.setItem('text_id', String(id));
+    router.push(`/comments/${id}`);
+    store.state.user.text_id = id;
+  };
+
+  const state = store.state;
+
 </script>
 
 <template>
+
   <!-- <h1>{{ msg }}</h1> -->
   <h3>Projects</h3>
   <div class="center-column">
     <div class="left-column">
-  <!-- <p>See <code>README.md</code> for more information.</p> -->
-  <div v-for="(value, key) in data" :key="key" style="padding:.5rem;" :title="value.meta">
-     <!-- <n-button type="info">Info</n-button> -->
-    <n-button @click="goToText(value.id)" :type="value.id === store.state.user.text_id ? 'info': ''">
-      {{value.author}}.&nbsp;{{value.title}}
-      <!-- <n-text type="info">{{value.author}}</n-text>.&nbsp;
-      «<n-text strong>{{value.title}}</n-text>» -->
-    </n-button>
+      <!-- <p>See <code>README.md</code> for more information.</p> -->
+      <div v-for="(value, key) in data" :key="key" style="padding:.5rem;" :title="value.meta">
+        <!-- <n-button type="info">Info</n-button> -->
+        <n-button @click="goToText(value.id)" :type="value.id === store.state.user.text_id ? 'info': ''">
+          {{value.author}}.&nbsp;{{value.title}}
+          <!-- <n-text type="info">{{value.author}}</n-text>.&nbsp;
+        «<n-text strong>{{value.title}}</n-text>» -->
+        </n-button>
+      </div>
+      <!-- <button type="button" @click="count++">count is: {{ count }}</button> -->
+      <n-divider style="width:300px;text-align: center; margin:auto;padding:1rem;" />
+      <n-descriptions label-placement="top" bordered :column="3" size="small">
+        <n-descriptions-item label="Client">{{store.version}}</n-descriptions-item>
+        <n-descriptions-item label="Server">{{state.user.server}}</n-descriptions-item>
+        <n-descriptions-item label="Commit">
+          <n-button text
+                    tag="a"
+                    :href="'https://github.com/yaskevich/elemental/commit/' + state.user.commit"
+                    target="_blank"
+                    type="primary">{{state.user.commit}}
+          </n-button>
+        </n-descriptions-item>
+      </n-descriptions>
+    </div>
   </div>
-  <!-- <button type="button" @click="count++">count is: {{ count }}</button> -->
-  <n-divider style="width:300px;text-align: center; margin:auto;padding:1rem;" />
-  <n-descriptions label-placement="top" bordered :column="3" size="small">
-    <n-descriptions-item label="Client">{{store.version}}</n-descriptions-item>
-    <n-descriptions-item label="Server">{{state.user.server}}</n-descriptions-item>
-    <n-descriptions-item label="Commit"><a type="primary" :href="'https://github.com/yaskevich/persona/commit/' + state.user.commit" target="_blank">{{state.user.commit}}</a></n-descriptions-item>
 
-  </n-descriptions>
-</div>
-</div>
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
-}
 
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
+  a {
+    color: #42b983;
+  }
 
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
+  label {
+    margin: 0 0.5em;
+    font-weight: bold;
+  }
+
+  code {
+    background-color: #eee;
+    padding: 2px 4px;
+    border-radius: 4px;
+    color: #304455;
+  }
+
 </style>
