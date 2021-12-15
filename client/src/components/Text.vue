34b972773b6b2a90b83a2056002f1db396705b84
yaskevich <alyaxey.yaskevich@gmail.com>
2021-12-15 13:12:05 +0300

    handle switch states ot the tools (selection/inspection, sequence/single unit) in Text [client]

--- client/src/components/Text.vue
+++ client/src/components/Text.vue
@@ -17,8 +17,20 @@
     p: number;
     s: number;
     comments: Array<number>;
-  };
+  }
 
+  interface IOption {
+    value: number;
+    label: string;
+  }
+
+  interface IRailStyle {
+    focused: boolean;
+    checked: boolean;
+  }
+
+  const singleMode = ref(false);
+  const inspectMode = ref(false);
   const isLoaded = ref(false);
   const commentsObject = reactive({});
   const text = reactive([] as Array<IToken>);
@@ -28,29 +40,30 @@
   const selectedCommentId = ref(0 as number);
   const selectedCommentTitle = ref('' as string);
   const selectedArray = ref([] as Array<IToken>);
-  const showDrawer = computed(() => Boolean(token1.value?.id && token2.value?.id));
-
-  interface IOption {
-    value: number;
-    label: string;
-  };
+  const showDrawer = computed(() =>
+    singleMode.value ? Boolean(token1.value?.id) : Boolean(token1.value?.id && token2.value?.id)
+  );
 
   onBeforeMount(async () => {
-      const data = await store.get('text', String(id));
-      Object.assign(text, data);
-      const textComments = await store.get('textcomments', String(id));
-      Object.assign(commentsObject, ...textComments.map((item:any) => ({ [item.id]: item })));
-      isLoaded.value = true;
+    const data = await store.get('text', String(id));
+    Object.assign(text, data);
+    const textComments = await store.get('textcomments', String(id));
+    Object.assign(commentsObject, ...textComments.map((item: any) => ({ [item.id]: item })));
+    isLoaded.value = true;
   });
 
   // const drawerUpdated = (show:boolean) => {
   const drawerUpdated = () => {
-    console.log('is drawer seen?', showDrawer.value);
+    // console.log('is drawer seen?', showDrawer.value);
     if (showDrawer.value) {
-      const min = Math.min(token1.value.id, token2.value.id);
-      const max = Math.max(token1.value.id, token2.value.id);
-      // console.log(min, max);
-      selectedArray.value = text.filter((x: IToken) => x.id >= min && x.id <= max);
+      if (singleMode.value) {
+        selectedArray.value = [token1.value];
+      } else {
+        const min = Math.min(token1.value.id, token2.value.id);
+        const max = Math.max(token1.value.id, token2.value.id);
+        // console.log(min, max);
+        selectedArray.value = text.filter((x: IToken) => x.id >= min && x.id <= max);
+      }
     }
   };
 
@@ -59,81 +72,100 @@
     token1.value = token2.value = {} as IToken;
   };
 
-  const bindTokensToComment = async() => {
-    const ids = selectedArray.value.map((x:IToken)=>x.id);
-    const {data} = await store.post('strings', {tokens: ids, id: selectedCommentId.value });
+  const bindTokensToComment = async () => {
+    const ids = selectedArray.value.map((x: IToken) => x.id);
+    const { data } = await store.post('strings', { tokens: ids, id: selectedCommentId.value });
     if (ids.length === data?.length) {
-      for (let item of selectedArray.value){
+      for (let item of selectedArray.value) {
         item.comments.push(selectedCommentId.value);
       }
-      commentsObject[String(selectedCommentId.value)] = { id: selectedCommentId.value, title: selectedCommentTitle.value };
+      commentsObject[String(selectedCommentId.value)] = {
+        id: selectedCommentId.value,
+        title: selectedCommentTitle.value,
+      };
 
       clearSelection();
     } else {
-      console.error("bind", ids, data);
+      console.error('bind', ids, data);
     }
   };
 
-  const selectOption = (id:number) => {
+  const selectOption = (id: number) => {
     // console.log("sel comment id", id);
     selectedCommentId.value = id;
-    selectedCommentTitle.value = options.value.filter((x:IOption) => x.value === id)[0]["label"];
+    selectedCommentTitle.value = options.value.filter((x: IOption) => x.value === id)[0]['label'];
   };
 
-  const queryDatabase = async (chunk:string) => {
+  const queryDatabase = async (chunk: string) => {
     if (chunk && chunk.length > 1) {
       const matches = await store.get('titles', String(2), { chunk: chunk });
       // console.log("matches", matches, "vs", chunk);
-      options.value = matches.map((x:any) => ({ label: x.title, value: x.id } as IOption));
+      options.value = matches.map((x: any) => ({ label: x.title, value: x.id } as IOption));
     }
   };
 
   const selectToken = (token: IToken) => {
-    // console.log("token", token);
-    if (token?.checked) {
-      console.log('de-select', token);
-      if (token1.value?.id === token.id) {
-        token1.value = token2.value;
-        token2.value = {} as IToken;
-      } else {
-        token2.value = {} as IToken;
-      }
-      token.checked = false;
+    if (inspectMode.value) {
+      console.log('inspect');
     } else {
-      // console.log("select", token);
-      if (!token1.value?.id) {
-        token.checked = true;
-        token1.value = token;
-      } else {
-        if (token2.value?.id && token.s === token1.value.s && token.s === token2.value.s) {
-          // console.log("set!");
-          const d1 = Math.abs(token.id - token1.value.id);
-          const d2 = Math.abs(token.id - token2.value.id);
-          if (d1 < d2) {
-            token.checked = true;
-            token1.value.checked = false;
-            token1.value = token;
-          } else {
-            token.checked = true;
-            token2.value.checked = false;
-            token2.value = token;
-          }
-          // console.log("d1", d1);
-          // console.log("d2", d2);
-        } else if (token.s === token1.value.s) {
-          token2.value = token;
-          token.checked = true;
-          // console.log("sel", token1.value);
+      // console.log("token", token);
+      if (token?.checked) {
+        console.log('de-select', token);
+        if (token1.value?.id === token.id) {
+          token1.value = token2.value;
+          token2.value = {} as IToken;
         } else {
-          console.log('nope!');
+          token2.value = {} as IToken;
+        }
+        token.checked = false;
+      } else {
+        // console.log("select", token);
+        if (!token1.value?.id) {
+          token.checked = true;
+          token1.value = token;
+        } else {
+          if (token2.value?.id && token.s === token1.value.s && token.s === token2.value.s) {
+            // console.log("set!");
+            const d1 = Math.abs(token.id - token1.value.id);
+            const d2 = Math.abs(token.id - token2.value.id);
+            if (d1 < d2) {
+              token.checked = true;
+              token1.value.checked = false;
+              token1.value = token;
+            } else {
+              token.checked = true;
+              token2.value.checked = false;
+              token2.value = token;
+            }
+            // console.log("d1", d1);
+            // console.log("d2", d2);
+          } else if (token.s === token1.value.s) {
+            token2.value = token;
+            token.checked = true;
+            // console.log("sel", token1.value);
+          } else {
+            console.log('nope!');
+          }
         }
       }
     }
   };
 
-  interface IRailStyle {
-    focused: boolean;
-    checked: boolean;
+  const railStyle = (stateObject: IRailStyle) => {
+    const { focused, checked }: IRailStyle = stateObject;
+    const style = {} as any;
+    if (checked) {
+      style.background = '#d03050';
+      if (focused) {
+        style.boxShadow = '0 0 0 2px #d0305040';
+      }
+    } else {
+      style.background = '#2080f0';
+      if (focused) {
+        style.boxShadow = '0 0 0 2px #2080f040';
+      }
+    }
+    return style;
   };
 
 </script>
@@ -142,12 +174,22 @@
 
   <div class="center-column" v-if="isLoaded">
     <div class="left-column">
-      <template v-for="(token, index) in text" :key="token.id" style="padding:.5rem;">
-        <div v-if="index && token.p !== text[index-1].p" style="margin-bottom:1rem;"></div>
-        <button :class="`text-button ${token?.checked ?'selected-button':''}  ${token?.comments?.length? 'commented': ''}`" size="small" :title="token.comments.map(x=>commentsObject[String(x)]['title']).join('•')" v-if="token.meta !== 'ip'"   :disabled="token.meta === 'ip+'" @click="selectToken(token)" >
-          {{token.form}}<sup v-if="token?.comments?.length">{{token.comments.length}}</sup>
-        </button>
-        </template>
+      <n-space justify="space-between" style="margin-right: 2rem;margin-left: 2rem;">
+        <n-button :color="inspectMode?'#ff69b4':'#8a2be2'" size="tiny" @click="inspectMode = !inspectMode">{{inspectMode?'Inspect':'Select'}}</n-button>
+        <n-switch :round="false" :rail-style="railStyle" v-model:value="singleMode" :style="`visibility: ${!inspectMode? 'visible': 'hidden'}`">
+          <template #checked>Unit</template>
+          <template #unchecked>Sequence</template>
+        </n-switch>
+      </n-space>
+      <n-divider style="text-align: center; margin:auto;padding:1rem;" />
+      <div>
+        <template v-for="(token, index) in text" :key="token.id" style="padding:.5rem;">
+            <div v-if="index && token.p !== text[index-1].p" style="margin-bottom:1rem;"></div>
+            <button :class="`text-button ${token?.checked ?'selected-button':''}  ${token?.comments?.length? 'commented': ''}`" size="small" :title="token.comments.map(x=>commentsObject[String(x)]['title']).join('•')" v-if="token.meta !== 'ip'"   :disabled="token.meta === 'ip+'" @click="selectToken(token)" >
+            {{token.form}}<sup v-if="token?.comments?.length">{{token.comments.length}}</sup>
+            </button>
+          </template>
+      </div>
       <n-divider style="width:300px;text-align: center; margin:auto;padding:1rem;" />
 
       <n-drawer v-model:show="showDrawer" placement="bottom" :on-update:show="drawerUpdated()">
@@ -155,11 +197,14 @@
           <n-space vertical>
             <div style="margin: 5px;">
               <template v-for="item in selectedArray" :key="item.id">
-                <span v-if="item.meta !== 'ip'" class="sequence selected-button text-button">{{item.form}}</span>
-              </template>
+                  <span v-if="item.meta !== 'ip'" class="sequence selected-button text-button">{{item.form}}</span>
+                </template>
             </div>
-            <n-auto-complete clearable :options="options" placeholder="Comment title" :on-update:value="(userInput: string) => queryDatabase(userInput)" :on-select="(selectedValue: number) => selectOption(selectedValue)"
-            />
+            <n-auto-complete clearable
+                             :options="options"
+                             placeholder="Comment title"
+                             :on-update:value="(userInput: string) => queryDatabase(userInput)"
+                             :on-select="(selectedValue: number) => selectOption(selectedValue)" />
             <div v-if="selectedCommentId" style="margin: 5px;">
               Comment: {{selectedCommentTitle}}
             </div>
