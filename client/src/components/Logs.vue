7876d6d7ae0291f17cd727bb237aa17bf69bdd0a
yaskevich <alyaxey.yaskevich@gmail.com>
2021-12-20 21:18:03 +0300

    create Logs component [client]

---
+++ client/src/components/Logs.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+  import { ref, reactive, onBeforeMount } from 'vue';
+  import store from '../store';
+  import router from '../router';
+
+  const data = ref();
+
+  onBeforeMount(async () => {
+
+  });
+
+</script>
+
+<template>
+
+  <h3>Logs</h3>
+  <div class="center-column">
+    <div class="left-column">
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
