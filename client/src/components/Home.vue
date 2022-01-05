564ba7a8315743cee219aec78631eaf5c7e3018d
yaskevich <alyaxey.yaskevich@gmail.com>
2022-01-05 19:20:54 +0300

    save ID of selected text in database in Home [client]

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -14,12 +14,20 @@
     data.value = await store.get('texts');
   });
 
-  const goToText = (id: number, title: string) => {
+  const goToText = async(id: number, title: string) => {
     // console.log("go to text", id);
-    localStorage.setItem('text_id', String(id));
-    router.push(`/comments/${id}`);
-    store.state.user.text_id = id;
-    document.title = title;
+    // localStorage.setItem('text_id', String(id));
+    if (id && store?.state?.user){
+      const { data } = await store.post('user/text', { id: id });
+      console.log("change text", data);
+      if (data?.id){
+        store.state.user.text_id = id;
+        document.title = title;
+        router.push(`/comments/${id}`);
+      } else {
+        console.error("error", data);
+      }
+    }
   };
 
   const state = store.state;
@@ -35,7 +43,7 @@
       <!-- <p>See <code>README.md</code> for more information.</p> -->
       <div v-for="(value, key) in data" :key="key" style="padding:.5rem;" :title="value.meta">
         <!-- <n-button type="info">Info</n-button> -->
-        <n-button @click="goToText(value.id, value.title)" :type="value.id === store.state.user.text_id ? 'info': ''">
+        <n-button @click="goToText(value.id, value.title)" :type="value.id === store?.state?.user?.text_id ? 'info': ''">
           {{value.author}}.&nbsp;{{value.title}}
           <!-- <n-text type="info">{{value.author}}</n-text>.&nbsp;
         «<n-text strong>{{value.title}}</n-text>» -->
@@ -44,9 +52,9 @@
       <!-- <button type="button" @click="count++">count is: {{ count }}</button> -->
       <n-divider style="width:300px;text-align: center; margin:auto;padding:1rem;" />
       <n-descriptions label-placement="top" bordered :column="3" size="small">
-        <n-descriptions-item label="Client">{{store.version}}</n-descriptions-item>
-        <n-descriptions-item label="Server">{{state.user.server}}</n-descriptions-item>
-        <n-descriptions-item label="Commit">
+        <n-descriptions-item label="Client">{{store?.version}}</n-descriptions-item>
+        <n-descriptions-item label="Server">{{state?.user?.server}}</n-descriptions-item>
+        <n-descriptions-item label="Commit" v-if="state?.user?.commit">
           <n-button text
                     tag="a"
                     :href="'https://github.com/yaskevich/elemental/commit/' + state.user.commit"
