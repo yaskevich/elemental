5d5aeb71d0fe2a778dac2552bf60222febc419ca
yaskevich <alyaxey.yaskevich@gmail.com>
2022-03-25 21:31:08 +0300

    update router with Backups [client]

--- client/src/router.ts
+++ client/src/router.ts
@@ -9,72 +9,78 @@ import Comment from './components/Comment.vue';
 import Comments from './components/Comments.vue';
 import Text from './components/Text.vue';
 import Logs from './components/Logs.vue';
+import Backups from './components/Backups.vue';
 
 const routes = [
-	// {
-		// path: '/test',
-		// name: 'Test',
-		// component: () => import('./components/Test.vue'),
+  // {
+    // path: '/test',
+    // name: 'Test',
+    // component: () => import('./components/Test.vue'),
     // component: Test,
-	// },
-	{
-		path: '/',
-		alias: ['/home'],
-		name: 'Home',
-		component: Home,
-	},
-	{
-		path: '/text',
-		name: 'Text',
-		component: Text,
-		props: true,
-	},
-	{
-		path: '/register',
-		name: 'Register',
-		component: Register,
-	},
-	{
-		path: '/login',
-		name: 'Login',
-		component: Login,
-	},
-	{
-		path: '/tags',
-		name: 'Tags',
-		component: Tags,
-	},
-	{
-		path: '/issues',
-		name: 'Issues',
-		component: Issues,
-	},
-	{
-		path: '/comments/:id?',
-		name: 'Comments',
+  // },
+  {
+    path: '/',
+    alias: ['/home'],
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/text',
+    name: 'Text',
+    component: Text,
+    props: true,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/tags',
+    name: 'Tags',
+    component: Tags,
+  },
+  {
+    path: '/issues',
+    name: 'Issues',
+    component: Issues,
+  },
+  {
+    path: '/backups',
+    name: 'Backups',
+    component: Backups,
+  },
+  {
+    path: '/comments/:id?',
+    name: 'Comments',
     component: Comments,
-	},
-	{
-		path: '/comment/:id?',
-		component: Comment,
-		name: 'Comment',
-		props: true
-	},
-	{
-		path: '/preview/:id?',
-		component: Preview,
-		name: 'Preview',
-	},
-	{
-		path: '/logs/:id?',
-		component: Logs,
-		name: 'Logs',
-	},
+  },
+  {
+    path: '/comment/:id?',
+    component: Comment,
+    name: 'Comment',
+    props: true
+  },
+  {
+    path: '/preview/:id?',
+    component: Preview,
+    name: 'Preview',
+  },
+  {
+    path: '/logs/:id?',
+    component: Logs,
+    name: 'Logs',
+  },
 ];
 
 const router = createRouter({
-	history: createWebHistory(),
-	routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
