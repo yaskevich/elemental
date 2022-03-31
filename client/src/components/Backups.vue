c8c431d66fe521c820fa8f153026c925f9d377ae
yaskevich <alyaxey.yaskevich@gmail.com>
2022-03-31 21:22:19 +0300

    restyle template with NCard and NGrid in Backups [client]

--- client/src/components/Backups.vue
+++ client/src/components/Backups.vue
@@ -1,32 +1,30 @@
 <template>
 
-  <div class="center-column">
-    <div class="left-column" v-if="isLoaded">
+  <n-card title="Backups" :bordered="false" v-if="isLoaded">
 
-      <n-space justify="center">
-        <div>
-          <h3>Backups</h3>
-        </div>
-        <div>
-          <h3 style="margin-top:.7em;margin-left: 2em;">
-            <n-button type="info" dashed @click="makeBackup">+ make</n-button>
-          </h3>
-        </div>
-      </n-space>
+    <template #header-extra>
+      <n-button type="primary" @click="makeBackup">+ make</n-button>
+    </template>
 
-      <div style="max-width:300px;">
-        <n-alert title="No backups!" type="warning" v-if=!backups.length>
-          There are no backup snapshots of the database on the server. <br/>
-          It is recommended for data safety to make backups regularly and save them somewhere else.
-        </n-alert>
-      </div>
-
-      <div v-for="(item, index) in backups" :key="index" style="padding:.5rem;">
-        <n-button secondary type="primary" :loading="item.state" @click="downloadBackup(index)">{{item.filename.split('.').shift()}}</n-button>
-      </div>
-
+    <div style="margin: 0 auto;">
+      <n-alert title="No backups!" type="warning" v-if=!backups.length>
+        There are no backup snapshots of the database on the server. It is recommended to make backups regularly and store them in other safe places as well.
+      </n-alert>
     </div>
-  </div>
+
+    <n-grid :x-gap="12" :y-gap="8" :cols="3">
+      <n-gi v-for="(item, index) in backups" :key="index">
+        <n-button secondary type="primary" :loading="item.state" @click="downloadBackup(index)">{{item.filename.split('.').shift()}}</n-button>
+      </n-gi>
+    </n-grid>
+
+    <template #footer>
+      <!-- #footer -->
+    </template>
+    <template #action>
+      <!-- #action -->
+    </template>
+  </n-card>
 
 </template>
 
@@ -37,9 +35,9 @@
   import store from '../store';
 
   interface IBackup {
-     filename: string;
-     state: boolean;
-   };
+    filename: string;
+    state: boolean;
+  }
 
   const message = useMessage();
   const backups = reactive([] as Array<IBackup>);
@@ -51,28 +49,31 @@
     if (data?.error) {
       message.error(`Database backup error: ${data.error}`, { duration: 5000 });
     } else {
-      if (backups.filter(x => x.filename === data.file).length){
+      if (backups.filter(x => x.filename === data.file).length) {
         message.warning(`Database snapshot was succsesfully updated! File ${data.file}`, { closable: true });
       } else {
-        backups.unshift({filename: data.file, state: false});
+        backups.unshift({ filename: data.file, state: false });
         message.info(`Database snapshot was succsesfully created! File ${data.file}`, { closable: true });
       }
     }
   };
 
-  const downloadBackup = async(index: number) => {
+  const downloadBackup = async (index: number) => {
     const backupInfo = backups[index];
     backupInfo.state = true;
     const result = await store.getFile('backupfile', backupInfo.filename);
     backupInfo.state = false;
     if (result) {
-      message.error("Download error!", { duration: 5000 });
+      message.error('Download error!', { duration: 5000 });
     }
   };
 
   onBeforeMount(async () => {
     const data = await store.get('backups');
-    Object.assign(backups, data.reverse().map((fn:any) => ({ filename: fn, state: false })));
+    Object.assign(
+      backups,
+      data.reverse().map((fn: any) => ({ filename: fn, state: false }))
+    );
     isLoaded.value = true;
   });
 
