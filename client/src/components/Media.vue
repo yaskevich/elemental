a2e4b85b22d152ea4bb68369785db0b7ed134407
yaskevich <alyaxey.yaskevich@gmail.com>
2022-04-10 22:59:21 +0300

    create Media component [client]

---
+++ client/src/components/Media.vue
@@ -0,0 +1,28 @@
+<template>
+  <n-card title="Media management" v-if="isLoaded" :bordered="false" class="minimal left">
+    <n-upload action="/api/upload">
+      <n-upload-dragger>
+        <div style="margin-bottom: 12px">
+          <n-icon size="48" :depth="3">
+            <archive-icon />
+          </n-icon>
+        </div>
+        <n-text style="font-size: 16px">
+          Click or drag an image to this area to upload
+        </n-text>
+        <n-p depth="3" style="margin: 8px 0 0 0">
+          Maximum file size is 2MB.
+        </n-p>
+      </n-upload-dragger>
+    </n-upload>
+  </n-card>
+</template>
+
+<script setup lang="ts">
+
+  import { ref, reactive, onBeforeMount } from 'vue';
+  import store from '../store';
+  import { ArchiveFilled as ArchiveIcon } from '@vicons/material';
+  const isLoaded = ref(true);
+
+</script>
