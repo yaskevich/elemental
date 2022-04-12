882f94dce80084b788f563e8fda3f0e544b40589
yaskevich <alyaxey.yaskevich@gmail.com>
2022-04-13 00:14:54 +0300

    enable Naive UI components [client]

--- client/src/main.ts
+++ client/src/main.ts
@@ -41,10 +41,13 @@ import {
   NGrid,
   NGi,
   NCard,
+  NUpload,
+  NUploadDragger,
+  NP,
 } from 'naive-ui'
 
 const naive = create({
-  components: [NButton, NText, NInput, NSwitch, NDivider, NAvatar, NIcon, NSpace, NInputGroup, NColorPicker, NMessageProvider, NForm, NFormItem, NRadioGroup, NRadio, NSelect, NDropdown, NTag, NSpin, NDataTable, NDescriptions, NDescriptionsItem, NInputNumber, NDrawer, NDrawerContent, NAutoComplete, NModal, NCheckbox, NCheckboxGroup, NTooltip, NPopconfirm, NAlert, NPageHeader, NGrid, NGi, NCard,]
+  components: [NButton, NText, NInput, NSwitch, NDivider, NAvatar, NIcon, NSpace, NInputGroup, NColorPicker, NMessageProvider, NForm, NFormItem, NRadioGroup, NRadio, NSelect, NDropdown, NTag, NSpin, NDataTable, NDescriptions, NDescriptionsItem, NInputNumber, NDrawer, NDrawerContent, NAutoComplete, NModal, NCheckbox, NCheckboxGroup, NTooltip, NPopconfirm, NAlert, NPageHeader, NGrid, NGi, NCard, NUpload, NUploadDragger, NP,]
 })
 
 createApp(App)
