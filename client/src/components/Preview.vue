a15485d143bc02688adfcd07d8c66d9487707c27
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-06-01 20:35:51 +0300

    set styles for custom tags in Preview [client]

--- client/src/components/Preview.vue
+++ client/src/components/Preview.vue
@@ -1,12 +1,14 @@
 <template>
-
   <div style="max-width:600px;margin: 0 auto;text-align: left; padding: 0 15px 10px 15px">
-    <h2><span style="color:lightgray;">({{entry.priority}})</span> {{entry.title}}</h2>
+    <h2>
+      <span style="color:lightgray;">({{ entry.priority }})</span>
+      {{ entry.title }}
+    </h2>
     <div>
       <n-divider title-placement="left">
         <span class="zone">translation</span>
       </n-divider>
-      <p style="margin-top:-1rem;font-style:italic;">{{entry.trans}}</p>
+      <p style="margin-top:-1rem;font-style:italic;">{{ entry.trans }}</p>
       <n-divider title-placement="left">
         <span class="zone">brief comment</span>
       </n-divider>
@@ -22,65 +24,104 @@
       <n-divider />
     </div>
   </div>
-
 </template>
 
 <script setup lang="ts">
 
-  import store from '../store';
-  import { ref, reactive, onBeforeMount } from 'vue';
-  import { useRoute } from 'vue-router';
+import store from '../store';
+import { ref, reactive, onBeforeMount } from 'vue';
+import { useRoute } from 'vue-router';
 
-  const vuerouter = useRoute();
-  const id = vuerouter.params.id;
+const vuerouter = useRoute();
+const id = vuerouter.params.id;
 
-  interface IEntry {
-    long_json: Object;
-    long_html: string;
-    long_text: string;
-    brief_json: Object;
-    brief_html: string;
-    brief_text: string;
-    text_id: number;
-    id: number;
-    issues: Array<number>;
-    tags: Array<number>;
-    priority: number;
-    published: boolean;
-    trans: string;
-    title: string;
-  }
+interface IEntry {
+  long_json: Object;
+  long_html: string;
+  long_text: string;
+  brief_json: Object;
+  brief_html: string;
+  brief_text: string;
+  text_id: number;
+  id: number;
+  issues: Array<number>;
+  tags: Array<number>;
+  priority: number;
+  published: boolean;
+  trans: string;
+  title: string;
+}
 
-  const entry: IEntry = reactive({}) as IEntry;
+const entry: IEntry = reactive({}) as IEntry;
 
-  onBeforeMount(async () => {
-    if (id) {
-      const data = await store.get(`comment/${id}`);
-      console.log('data from server', data);
-      if (data.length) {
-        console.log('data', data);
-        Object.assign(entry, data[0]);
-      }
+onBeforeMount(async () => {
+  if (id) {
+    const data = await store.get(`comment/${id}`);
+    console.log('data from server', data);
+    if (data.length) {
+      console.log('data', data);
+      Object.assign(entry, data[0]);
     }
-  });
+  }
+});
 
 </script>
 
 <style scoped lang="scss">
 :deep(var.error) {
-  display:none;
+  display: none;
 }
 
-:deep(img){
-  max-width:370px;
+:deep(var.name) {
+  font-weight: bold;
+  font-style: normal;
+  background-color: yellow;
+  color: #333;
+  padding: 3px;
+  border-radius: 8px;
+}
+
+:deep(var.example) {
+  /* background-color: rgb(246, 248, 250); */
+  background-color: rgb(227, 232, 244);
+  border-radius: 8px;
+  color: rgb(36, 51, 90);
+  font-size: 14px;
+  line-height: 1.4em;
+  padding: 3px;
+}
+
+:deep(var.book) {
+  background: #eee;
+  padding: 0 3px;
+  color: #c76c0c;
+}
+
+:deep(blockquote) {
+  background: #f9f9f9;
+  border-left: 10px solid #ccc;
+  margin: 1.5em 10px;
+  padding: 0.5em 10px;
+  quotes: "\201C""\201D""\2018""\2019";
+}
+
+:deep(blockquote):before {
+  color: #ccc;
+  content: open-quote;
+  font-size: 4em;
+  line-height: 0.1em;
+  margin-right: 0.25em;
+  vertical-align: -0.4em;
+}
+
+:deep(blockquote p) {
+  display: inline;
+}
+
+:deep(img) {
+  max-width: 370px;
   max-height: 300px;
 }
-
-.zone {
-  border: 1px solid lightgray;padding: 0px 5px 0px 5px;font-size: smaller; border-radius: 10% / 50%;
-  font-variant: small-caps;
-}
-
 :deep(img + p.caption) {
   color: gray;
   font-weight: bold;
