d9aac3ddc3a81326bd0708028e1a15a0b13091c3
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-06-07 21:15:55 +0300

    restyle Tags [client]

--- client/src/components/Tags.vue
+++ client/src/components/Tags.vue
@@ -1,75 +1,89 @@
 <template>
+  <n-card title="Tags" :bordered="false" class="minimal left" v-if="isLoaded">
+    <template #header-extra>
+      <n-button v-if="!showForm" type="primary" @click="showForm = true;">+ new</n-button>
+    </template>
 
-  <h3>Tags</h3>
-  <n-space vertical>
-    <n-input style="min-width:250px;" autosize v-model:value="newTag.en" type="text" placeholder="English title" />
-    <n-input autosize style="min-width:250px;" v-model:value="newTag.ru" type="text" placeholder="Russian title" />
-    <n-button type="info" dashed @click="editTag(newTag)">Create new tag +</n-button>
-  </n-space>
-  <n-divider style="width:300px;text-align: center; margin:auto;padding:1rem;" />
-  <div v-if="!tags.length">
-    <n-text type="error">There are no tags!</n-text>
-  </div>
-  <div class="center-column">
-    <div class="left-column">
+    <n-input-group
+      style="display:block;max-width:300px;text-align:center;margin: 0 auto"
+      v-if="showForm"
+    >
       <n-space vertical>
-        <n-input-group v-for="item in tags" :key="item.id" style="display:block;">
-          <n-space justify="space-between">
-            <div>
-            <n-input autosize v-model:value="item.en" placeholder="English title" />
-            <n-input autosize v-model:value="item.ru" placeholder="Russian title" />
-            </div>
-            <n-button type="primary" ghost @click="editTag(item)">Save</n-button>
-          </n-space>
-        </n-input-group>
-      </n-space>
-    </div>
-  </div>
+        <n-input v-model:value="newTag.en" type="text" placeholder="English title" />
+        <n-input v-model:value="newTag.ru" type="text" placeholder="Russian title" />
 
+        <n-space justify="center">
+          <n-button tertiary type="warning" @click="showForm = false;">Cancel</n-button>
+          <n-button type="info" @click="editTag(newTag)">Save</n-button>
+        </n-space>
+      </n-space>
+      <n-divider style="width:300px;text-align: center; margin:auto;padding:1rem;" />
+    </n-input-group>
+
+    <div v-if="!tags.length">
+      <n-text type="error">There are no tags!</n-text>
+    </div>
+
+    <n-space vertical size="large" align="end">
+      <n-grid x-gap="12" cols="3" responsive="screen" v-for="item in tags" :key="item.id">
+        <n-gi>
+          <n-input v-model:value="item.en" placeholder="English title" />
+        </n-gi>
+        <n-gi>
+          <n-input v-model:value="item.ru" placeholder="English title" />
+        </n-gi>
+        <n-gi style="text-align:right;">
+          <n-button type="info" @click="editTag(item)">Save</n-button>
+        </n-gi>
+      </n-grid>
+    </n-space>
+  </n-card>
 </template>
 
 <script setup lang="ts">
 
-  import store from '../store';
-  import { ref, reactive, onBeforeMount } from 'vue';
+import store from '../store';
+import { ref, reactive, onBeforeMount } from 'vue';
 
-  interface ITag {
-    id?: number,
-    en: string,
-    ru: string,
-  };
+interface ITag {
+  id?: number,
+  en: string,
+  ru: string,
+};
 
-  const newTag = reactive({ en: '', ru: '' });
-  const tags:Array<ITag> = reactive([] as Array<ITag>);
+const newTag = reactive({ en: '', ru: '' });
+const tags: Array<ITag> = reactive([] as Array<ITag>);
+const showForm = ref(false);
+const isLoaded = ref(false);
 
-  onBeforeMount(async () => {
-    const data = await store.get('tags');
-    Object.assign(tags, data);
-    console.log('data from server', data);
-  });
+onBeforeMount(async () => {
+  const data = await store.get('tags');
+  Object.assign(tags, data);
+  // console.log('data from server', data);
+  isLoaded.value = true;
+});
 
-  const editTag = async (tag:ITag) => {
-    console.log('edit tag', tag);
-    const params: ITag = { } as ITag;
-    if (tag?.id) {
-      params.id = tag.id;
+const editTag = async (tag: ITag) => {
+  console.log('edit tag', tag);
+  const params: ITag = {} as ITag;
+  if (tag?.id) {
+    params.id = tag.id;
+  }
+  params.en = tag.en;
+  params.ru = tag.ru;
+  const result = await store.post('tag', params);
+  if (result?.data?.id) {
+    params.id = result.data.id;
+    if (!tag?.id) {
+      tags.push(params);
+      newTag.en = newTag.ru = '';
     }
-    params.en = tag.en;
-    params.ru = tag.ru;
-    const result = await store.post('tag', params);
-    if (result?.data?.id) {
-      params.id = result.data.id;
-      if (!tag?.id) {
-        tags.push(params);
-        newTag.en = newTag.ru = '';
-      }
-    } else {
-      console.log('error', result);
-    }
-  };
+  } else {
+    console.log('error', result);
+  }
+};
 
 </script>
 
 <style scoped>
-
 </style>
