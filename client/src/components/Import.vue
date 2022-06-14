bbb2a32e7cf1ac799acc17f5a5e3ca2afacb7097
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-06-14 15:17:48 +0300

    render processing options in Import [client]

--- client/src/components/Import.vue
+++ client/src/components/Import.vue
@@ -3,13 +3,16 @@
 import { ref, reactive, onBeforeMount } from 'vue';
 import store from '../store';
 import router from '../router';
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { useMessage } from 'naive-ui';
 
-
+const message = useMessage();
 const text = ref('');
-// const showForm = ref(false);
+const link = ref('');
 
+const sourceMode = ref('text');
 const importMode = ref('rich');
+
 const items = [
     {
         value: "rich",
@@ -19,19 +22,48 @@ const items = [
     {
         value: 'plain',
         label: 'Plain Text',
-        disabled: true,
+        disabled: false,
     },
     {
         value: 'xml',
         label: 'XML',
-        disabled: true,
+        disabled: false,
     },
+];
+
+const sources = [
+    {
+        value: "text",
+        label: "Text Input",
+        disabled: false,
+    },
+    {
+        value: 'url',
+        label: 'URL',
+        disabled: false,
+    }
 ];
 
 const startProcessing = async () => {
     // .trim()
-    // const response = await axios.get("https://raw.githubusercontent.com/dracor-org/greekdracor/main/tei/aristophanes-frogs.xml");
-    // console.log(response);
+    if (sourceMode.value === "text") {
+
+    } else { // if URL
+        const url = link.value.trim();
+        console.log(`URL [${url}]`);
+        try {
+            // https://raw.githubusercontent.com/dracor-org/greekdracor/main/tei/aristophanes-frogs.xml
+            const response = await axios.get(url);
+            console.log(response);
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                // : unknown | Error | AxiosError
+                message.error(`URL fetch error. ${error?.message}`, { duration: 5000 });
+            }
+            console.log(error);
+        }
+    }
+
 };
 
 onBeforeMount(async () => {
@@ -47,10 +79,20 @@ onBeforeMount(async () => {
 <template>
     <n-card title="Importing Text" :bordered="false" class="minimal left">
         <template #header-extra>
-            <n-button icon-placement="left" type="primary">Start</n-button>
+            <n-button icon-placement="left" type="primary" @click="startProcessing">Start</n-button>
         </template>
         <n-space vertical size="large">
-            <n-input maxlength="30" clearable placeholder="Insert URL here..." />
+            <n-radio-group v-model:value="sourceMode" name="radiogroup">
+                <n-space>
+                    <n-radio
+                        v-for="item in sources"
+                        :key="item.value"
+                        :value="item.value"
+                        :label="item.label"
+                        :disabled="item.disabled"
+                    />
+                </n-space>
+            </n-radio-group>
             <n-radio-group v-model:value="importMode" name="radiogroup">
                 <n-space>
                     <n-radio
@@ -62,6 +104,13 @@ onBeforeMount(async () => {
                     />
                 </n-space>
             </n-radio-group>
+            <n-input
+                v-model:value="link"
+                clearable
+                spellcheck="false"
+                placeholder="Insert URL here..."
+                v-if="sourceMode === 'url'"
+            />
 
             <n-input
                 v-model:value="text"
@@ -71,6 +120,7 @@ onBeforeMount(async () => {
                     minRows: 3
                 }"
                 placeholder="Insert text here..."
+                v-if="sourceMode === 'text'"
             />
         </n-space>
     </n-card>
