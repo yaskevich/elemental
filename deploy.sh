3f731f28f2e6d1654fb78bafb918cc4e74dd8596
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-07-09 10:42:05 +0300

    update script for automatic deployment

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,28 @@
 USER=yaskevich
-APP=elemental
+APP=flowercat
 BRANCH=master
 PROD=production
 DIR=$(pwd)
 WORK=$DIR/$PROD/$APP
+UUID=$(cat /proc/sys/kernel/random/uuid)
+TEMP=$DIR/$APP-$UUID
+
+mkdir $TEMP -p
+
+mv $WORK/backup/ $TEMP
+mv $WORK/images/ $TEMP
 
 pm2 delete $APP
 rm -rf $DIR/$PROD/$APP
 # degit $USER/$APP#$BRANCH $WORK
-git clone https://github.com/$USER/$APP  $WORK --depth 1
+git clone https://github.com/$USER/$APP $WORK --depth 1
 HASH=$(git -C $WORK rev-parse --short HEAD)
 # echo $HASH
 
 rm $WORK/* 2>/dev/null
 rm $WORK/.* 2>/dev/null
 npm install --prefix $WORK/client
-npm run build  --prefix $WORK/client
+npm run build --prefix $WORK/client
 mv $WORK/client/dist $WORK/public
 
 cd $WORK/server
@@ -29,6 +36,11 @@ done
 npm install --prefix $WORK
 cp $DIR/$APP.env $WORK/.env
 printf "\nCOMMIT=%s" $HASH >> $WORK/.env
+
+mv $TEMP/backup $WORK
+mv $TEMP/images $WORK
+rm -rf $TEMP
+
 cd $WORK
 rm -rf $WORK/client $WORK/server
 # # https://pm2.keymetrics.io/docs/usage/application-declaration/#ecosystem-file
