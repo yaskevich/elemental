6d5ae7fb6d791c89ff38af56edfdd27f05741dec
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-07-11 14:01:28 +0300

    hide form after successful registration in Register [client]

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,15 +1,15 @@
 <template>
-
   <div class="center-column">
     <h3>Registration</h3>
     <div class="left-column">
-
-      <n-form :label-width="80"
-              :model="formValue"
-              :rules="rules"
-              :size="size"
-              ref="formRef">
-
+      <n-form
+        :label-width="80"
+        :model="formValue"
+        :rules="rules"
+        :size="size"
+        ref="formRef"
+        v-if="showForm"
+      >
         <n-form-item label="Username (nick). English letters only" path="user.username">
           <n-input v-model:value="formValue.user.username" placeholder="Input username" />
         </n-form-item>
@@ -26,103 +26,108 @@
         </n-form-item>
 
         <n-form-item label="Grammatical gender" path="user.sex">
-          <n-select placeholder="Choose gender"
-                    :options="generalOptions"
-                    v-model:value="formValue.user.sex" />
+          <n-select
+            placeholder="Choose gender"
+            :options="generalOptions"
+            v-model:value="formValue.user.sex"
+          />
         </n-form-item>
 
         <n-form-item>
           <n-button @click="handleValidateClick">Submit</n-button>
         </n-form-item>
-
       </n-form>
-      <div v-if="note" style="padding-bottom: 2rem;">
-        <pre>
-              {{note}}
-            </pre>
-        <div>
+      <div v-if="note" class="center-column" style="padding-bottom: 2rem;max-width:300px;">
+        <p>
+          After the administartor activates your account, you will be able to login with the e-mail
+          <em>{{ formValue.user.email }}</em> and this password:
+        </p>
+        <div style="font-family: monospace;font-size:2rem;">{{ note }}</div>
+        <p>Copy the password and store it securely.</p>
+        <!-- <div>
           Copy password and go to
           <router-link to="/login">login page</router-link>.
-        </div>
+        </div>-->
       </div>
     </div>
   </div>
-
 </template>
 
 <script setup lang="ts">
 
-  import store from '../store';
-  import { ref, reactive, onBeforeMount } from 'vue';
-  import { FormInst } from 'naive-ui';
+import store from '../store';
+import { ref, reactive, onBeforeMount } from 'vue';
+import { FormInst } from 'naive-ui';
 
-  const formRef = ref<FormInst | null>(null);
-  const note = ref('');
-  const formValue = ref({
-    user: {
-      username: '',
-      firstname: '',
-      lastname: '',
-      email: '',
-      sex: undefined,
+const formRef = ref<FormInst | null>(null);
+const note = ref('');
+const showForm = ref(true);
+const formValue = ref({
+  user: {
+    username: '',
+    firstname: '',
+    lastname: '',
+    email: '',
+    sex: undefined,
+  },
+});
+
+const generalOptions = ['female', 'male'].map((v, i) => ({
+  label: v,
+  value: ++i,
+}));
+
+const size = ref('medium');
+
+const rules = {
+  user: {
+    username: {
+      required: true,
+      message: 'Please input your username',
+      trigger: 'blur',
     },
-  });
-
-  const generalOptions = ['female', 'male'].map((v, i) => ({
-    label: v,
-    value: ++i,
-  }));
-
-  const size = ref('medium');
-
-  const rules = {
-    user: {
-      username: {
-        required: true,
-        message: 'Please input your username',
-        trigger: 'blur',
-      },
-      firstname: {
-        required: true,
-        message: 'Please input your firstname',
-        trigger: 'blur',
-      },
-      lastname: {
-        required: true,
-        message: 'Please input your lastname',
-        trigger: 'blur',
-      },
-      sex: {
-        type: 'number',
-        required: true,
-        message: 'Please input your gender',
-        trigger: ['blur', 'change'],
-      },
-      email: {
-        type: 'email',
-        required: true,
-        message: 'Please input your e-mail',
-        trigger: 'blur',
-      },
+    firstname: {
+      required: true,
+      message: 'Please input your firstname',
+      trigger: 'blur',
     },
-  };
+    lastname: {
+      required: true,
+      message: 'Please input your lastname',
+      trigger: 'blur',
+    },
+    sex: {
+      type: 'number',
+      required: true,
+      message: 'Please input your gender',
+      trigger: ['blur', 'change'],
+    },
+    email: {
+      type: 'email',
+      required: true,
+      message: 'Please input your e-mail',
+      trigger: 'blur',
+    },
+  },
+};
 
-  const handleValidateClick = (e: MouseEvent) => {
-    e.preventDefault();
-    formValue.value.user.username = formValue.value.user.username.replace(/[^\x41-\x7F]/g, '');
-    if (formRef!.value) {
-      formRef.value.validate(async (errors: any) => {
-        if (!errors) {
-          // console.log(formValue.value);
-          const result = await store.postUnauthorized('user/reg', formValue.value.user);
-          const pwd = result?.data?.message;
-          console.log(pwd);
-          note.value = pwd;
-        } else {
-          console.log(errors);
-        }
-      });
-    }
-  };
+const handleValidateClick = (e: MouseEvent) => {
+  e.preventDefault();
+  formValue.value.user.username = formValue.value.user.username.replace(/[^\x41-\x7F]/g, '');
+  if (formRef!.value) {
+    formRef.value.validate(async (errors: any) => {
+      if (!errors) {
+        // console.log(formValue.value);
+        const result = await store.postUnauthorized('user/reg', formValue.value.user);
+        const pwd = result?.data?.message;
+        // console.log(pwd);
+        note.value = pwd;
+        showForm.value = false;
+      } else {
+        console.log(errors);
+      }
+    });
+  }
+};
 
 </script>
