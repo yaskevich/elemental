2bd018bdfbe401b11b670cdb58f3bc6b11847a1e
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-07-21 14:28:10 +0300

    create component Source [client]

---
+++ client/src/components/Source.vue
@@ -0,0 +1,187 @@
+<template>
+    <n-card title="Source" :bordered="false" class="minimal left" v-if="isLoaded">
+        <template #header-extra>
+            <!-- <n-checkbox v-model:checked="isBatch" style="margin-right:1rem;">Batch mode</n-checkbox> -->
+            <n-button type="primary" @click="save">Save</n-button>
+        </template>
+        <n-form ref="formRef" :label-width="80" :model="form" :rules="rules">
+            <n-space vertical>
+                <n-tag
+                    type="success"
+                    size="large"
+                    v-if="cjsObject?.data?.length === 1"
+                >{{ cjsObject.data[0].id }}</n-tag>
+                <n-alert
+                    title="Batch mode"
+                    type="warning"
+                    v-if="cjsObject?.data?.length > 1"
+                >You have put the dataset containing more than one BibTex record. The data will be processed in batch mode: the same language will be set for every record. After processing will be completed, you will be forwarded to the list with all sources.</n-alert>
+                <n-form-item label="Language variety" path="langLabel" style="display:block;">
+                    <n-auto-complete
+                        @select="onSelect"
+                        :get-show="() => { return true }"
+                        clearable
+                        :loading="langLoading"
+                        v-model:value="form.langLabel"
+                        :options="languages"
+                        placeholder="Language"
+                        @update:value="updateComplete"
+                    />
+                </n-form-item>
+                <n-form-item path="bib" label="BibTex">
+                    <n-input
+                        v-model:value="form.bib"
+                        type="textarea"
+                        clearable
+                        :autosize="{ minRows: 5, maxRows: 20 }"
+                        autofocus
+                        placeholder="Bibliography item in BibTex format..."
+                    />
+                </n-form-item>
+                <n-tag type="error" v-if="form.bib && !cjsObject?.data">BibTex error!</n-tag>
+
+                <div v-if="cjsObject?.data" v-html="cjsObject.format('bibliography', formatting)"></div>
+                <div v-if="!form.bib" style="color:darkorange">
+                    You can easily generate BibTex record via
+                    <n-button text tag="a" href="https://zbib.org" target="_blank" type="primary">
+                        zotero
+                        <strong>bib</strong>
+                    </n-button>
+                </div>
+            </n-space>
+        </n-form>
+    </n-card>
+</template>
+
+<script setup lang="ts">
+import store from '../store';
+import { ref, reactive, onBeforeMount, computed, unref, } from 'vue';
+import router from '../router';
+import { useRoute } from 'vue-router';
+import type { SelectOption, FormInst, FormItemRule, } from 'naive-ui';
+import { useMessage } from 'naive-ui';
+import Cite from 'citation-js';
+
+const message = useMessage();
+const vuerouter = useRoute();
+
+const formRef = ref<FormInst | null>(null);
+const isLoaded = ref(false);
+const form = reactive({ id: String(unref(vuerouter.params.id)), lang: '', langLabel: '', bib: '' });
+const languages = ref([] as Array<SelectOption>);
+const langLoading = ref(true);
+// ['apa', 'vanvouver', 'harvard1']
+const formatting = reactive({ format: 'html', template: 'apa', lang: 'en-US' });
+
+const rules = {
+    langLabel: {
+        required: true,
+        trigger: ['blur', 'change'],
+        message: 'Language should be set',
+    },
+    bib: {
+        validator: validateBib,
+        required: true,
+        trigger: ['blur', 'input'],
+        message: 'Valid BibTex format is expected',
+    },
+};
+
+const cjsObject = computed(() => { try { return new Cite(form.bib, { generateGraph: false }) } catch (error) { return new Cite('') } });
+
+function validateBib(rule: FormItemRule, value: string): boolean {
+    return Boolean(cjsObject?.value?.data?.length);
+};
+
+function validateLang(rule: FormItemRule, value: string): boolean {
+    return Boolean(cjsObject?.value?.data?.length);
+};
+
+const getLanguages = async (chunk?: string) => {
+    langLoading.value = true;
+    const langData = await store.get('languages', chunk);
+    languages.value = langData.map((x: any, i: number) => ({ tag: x.tag, label: `${x.name} (${x.location})`, value: i }));
+    langLoading.value = false;
+    // console.log(chunk, languages.value.length);
+};
+
+const updateComplete = async (chunk?: string) => {
+    // console.log("update:value", chunk, form.langLabel);
+    if (chunk && chunk.length > 1) {
+        // console.log("input", chunk);
+        await getLanguages(chunk);
+    }
+};
+
+const onSelect = (index: number) => {
+    // console.log("select");
+    const langId = languages.value[index]?.tag as string;
+    form.lang = langId;
+    formatting.lang = langId;
+};
+
+const setLanguage = async () => {
+    await getLanguages(form.lang);
+    const selectedLang = languages.value.filter(x => x.tag === form.lang).shift();
+    // console.log(form.lang, selectedLang, selectedLang?.label);
+    if (selectedLang?.label) {
+        form.langLabel = selectedLang.label as any;
+    }
+};
+
+const save = async (e: MouseEvent) => {
+    e.preventDefault();
+    try {
+        await setLanguage();
+        await formRef.value?.validate(async (errors) => {
+            if (!errors) {
+                const { data } = await store.post('source', { lang: form.lang, bib: cjsObject.value.data, id: form.id });
+                if (data?.error) {
+                    const note = data.error === `Key (citekey)=(${cjsObject?.value?.data?.[0]?.id}) already exists.` ? 'The publication with this ID already exists in the database. Please, pay attention to avoid duplicates!' : data.error;
+                    message.error(note, {duration: 5000});
+                } else {
+                    if (!form.id) {
+                        form.id = data?.[0]?.id;
+                        if (form.id) {
+                            router.replace('/source/' + form.id);
+                        }
+                    }
+                    if (data?.length > 1) {
+                        router.push('/sources');
+                    }
+                }
+                // console.log(data);
+            } else {
+                console.log(errors)
+            }
+        });
+    } catch (error) {
+        console.log("fail");
+    }
+};
+
+onBeforeMount(async () => {
+    if (form.id) {
+        const data = await store.get('source', form.id);
+        // console.log('data from server', data);
+        form.lang = data[0].lang;
+        form.bib = (new Cite(data[0].bibtex)).format('bibtex');
+    } else {
+        console.log("default language", store.state?.user?.text?.lang);
+        form.lang = unref(store.state?.user?.text?.lang) || '';
+        console.log("add new");
+    }
+    await setLanguage();
+    isLoaded.value = true;
+});
+
+</script>
+
+<style scoped lang="scss">
+:deep(.csl-entry) {
+    padding: 0.5rem;
+    background: rgba(171, 196, 207, 0.745);
+    border-radius: 3px;
+    margin-bottom: 1rem;
+}
+</style>
