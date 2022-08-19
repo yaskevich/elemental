b08e5c763aa8d3ca358511954551d843157ce454
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-08-19 23:57:10 +0300

    enable autoimport via Vite config [client]

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,36 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Checker from 'vite-plugin-checker'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Checker({ typescript: true })],
+  plugins: [vue(), Checker({ typescript: true }),
+  AutoImport({
+    imports: [
+      'vue',
+      {
+        'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar'],
+      },
+    ],
+  }),
+  Components({
+    resolvers: [NaiveUiResolver()]
+  })
+
+  ],
   server: {
     port: 3090,
     proxy: {
-     '/api': {
-       target: 'http://localhost:3091',
-       changeOrigin: true,
-       secure: false,
-       ws: true,
-     }
-   }
-  }
+      '/api': {
+        target: 'http://localhost:3091',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
+    }
+  },
+
 })
