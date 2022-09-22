c104c7b156279c0229d1ab66e63fcbb0243bce0f
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-09-22 14:48:54 +0300

    handle image filtering by name in Tiptap [client]

--- client/src/components/Tiptap.vue
+++ client/src/components/Tiptap.vue
@@ -5,8 +5,9 @@
     <button
       v-for="item in store?.state?.user?.classes"
       @click="customEditor.chain().focus().toggleMarker({ class: item.name }).run()"
-      :class="{ 'is-active': customEditor.isActive('marker', { class: item.name }), [item.name]: true }"
-    >{{ item.name }}</button>
+      :class="{ 'is-active': customEditor.isActive('marker', { class: item.name }), [item.name]: true }">
+      {{ item.name }}
+    </button>
 
     <!-- <button @click="customEditor.chain().focus().unsetAnnotation().run()" :disabled="!customEditor.isActive('annotation')">
               Clear Annotation
@@ -14,14 +15,16 @@
 
     <button
       @click="customEditor.chain().focus().toggleBlockquote().run()"
-      :class="{ 'is-active': customEditor.isActive('blockquote') }"
-    >blockquote</button>
+      :class="{ 'is-active': customEditor.isActive('blockquote') }">
+      blockquote
+    </button>
 
     <button
       @click="customEditor.commands.toggleBold()"
       :class="{ 'is-active': customEditor.isActive('bold') }"
-      style="font-weight:bold;"
-    >emphasized</button>
+      style="font-weight: bold">
+      emphasized
+    </button>
 
     <!-- style="background-color: #fac9ff;" -->
     <button @click="showImagesModal = true">+image</button>
@@ -31,20 +34,20 @@
       style="background-color: #fac9ff;"
     >caption</button>-->
 
-    <button @click="showSourcesModal = true" style="background-color: #CCFFFF;">+ref</button>
+    <button @click="showSourcesModal = true" style="background-color: #ccffff">+ref</button>
 
     <!-- <br /> -->
 
     <!-- clear formatting -->
-    <button
-      @click="customEditor.chain().focus().clearNodes().unsetAllMarks().run()"
-      style="background-color:white"
-    >clear</button>
+    <button @click="customEditor.chain().focus().clearNodes().unsetAllMarks().run()" style="background-color: white">
+      clear
+    </button>
 
     <button
       @click="customEditor.chain().focus().deleteSelection().run()"
-      style="background-color: #da1d1d;color:white"
-    >del</button>
+      style="background-color: #da1d1d; color: white">
+      del
+    </button>
 
     <n-modal
       v-model:show="showSourcesModal"
@@ -54,21 +57,19 @@
       title="Select a reference"
       :bordered="false"
       size="huge"
-      :segmented="{ content: 'soft', footer: 'soft' }"
-    >
+      :segmented="{ content: 'soft', footer: 'soft' }">
       <n-select
         clearable
         filterable
         :options="sources"
         placeholder="Bibliographic sources"
         @update:value="onSourceSelected"
-        style="margin-bottom: 15px;"
-      />
+        style="margin-bottom: 15px" />
 
-      <div v-html="html" v-if="html" style="margin-bottom: 15px;"></div>
+      <div v-html="html" v-if="html" style="margin-bottom: 15px"></div>
 
       <n-space justify="space-between">
-        <n-button type="info" @click="showSourcesModal = false;">Cancel</n-button>
+        <n-button type="info" @click="showSourcesModal = false">Cancel</n-button>
         <n-button type="success" @click="insertCitation" :disabled="!selectedSourceId">Insert</n-button>
       </n-space>
     </n-modal>
@@ -81,35 +82,36 @@
       title="Select an image"
       :bordered="false"
       size="huge"
-      :segmented="{ content: 'soft', footer: 'soft' }"
-    >
+      :segmented="{ content: 'soft', footer: 'soft' }">
       <!-- <template #header-extra>
         Oops!
       </template>-->
       <!-- Content -->
-      <n-image-group>
+      <n-space vertical>
+        <n-input v-model:value="titleToFilter" type="text" placeholder="Filter by image title" clearable />
         <n-space>
-          <n-image
-            v-for="(item, index) in images"
-            :key="index"
-            width="100"
-            class="selectable"
-            :preview-disabled="true"
-            :src="item.url"
-            @click="selectImage(item)"
-          />
+          <template
+            v-for="item in images.filter((x:any) => titleToFilter ? x.title.includes(titleToFilter) : true)"
+            :key="index">
+            <figure>
+              <img width="100" class="selectable" :preview-disabled="true" :src="item.url" @click="selectImage(item)" />
+              <figcaption class="modal-caption">
+                <n-ellipsis style="max-width: 100px">{{ item.title }}</n-ellipsis>
+              </figcaption>
+            </figure>
+          </template>
         </n-space>
-      </n-image-group>
+      </n-space>
       <!-- <template #footer>
         Footer
       </template>-->
     </n-modal>
     <div class="editorboard">
       <editor-content :editor="customEditor" :class="editorclass" />
-      <div
-        class="counter"
-        v-if="customEditor"
-      >{{ customEditor.storage.characterCount.characters() }} characters / {{ customEditor.storage.characterCount.words() }} words</div>
+      <div class="counter" v-if="customEditor">
+        {{ customEditor.storage.characterCount.characters() }} characters /
+        {{ customEditor.storage.characterCount.words() }} words
+      </div>
     </div>
   </div>
 </template>
@@ -121,12 +123,13 @@ import Cite from 'citation-js';
 import store from '../store';
 // import type { SelectOption } from 'naive-ui';
 
-const props = defineProps<{ editorclass: string, sources: any, images: any, content: any }>();
+const props = defineProps<{ editorclass: string; sources: any; images: any; content: any }>();
 
 const showImagesModal = ref(false);
 const showSourcesModal = ref(false);
 const html = ref('');
 const selectedSourceId = ref<number>();
+const titleToFilter = ref('');
 
 const customEditor = new Editor({
   content: '',
@@ -153,14 +156,22 @@ const customEditor = new Editor({
 const onSourceSelected = (index: number, option: IBib) => {
   // console.log(index, option);
   if (index !== null) {
-    html.value = (new Cite(option?.bibtex)).format('bibliography', { format: 'html', template: 'apa', lang: option.lang });
+    html.value = new Cite(option?.bibtex).format('bibliography', {
+      format: 'html',
+      template: 'apa',
+      lang: option.lang,
+    });
     selectedSourceId.value = option.id;
   }
 };
 
 const insertCitation = () => {
   showSourcesModal.value = false;
-  customEditor.chain().focus().setCitation({ id: selectedSourceId.value as number }).run();
+  customEditor
+    .chain()
+    .focus()
+    .setCitation({ id: selectedSourceId.value as number })
+    .run();
 };
 
 onBeforeUnmount(() => {
@@ -181,11 +192,7 @@ onBeforeUnmount(() => {
 
 const selectImage = (item: IImageItem) => {
   const title = item.title || '';
-  customEditor
-    .chain()
-    .focus()
-    .setFigure({ src: item.url, caption: title })
-    .run();
+  customEditor.chain().focus().setFigure({ src: item.url, caption: title }).run();
   // customEditor.chain().focus()
   //   .setImage({ src: item.url, alt: item.title, title: item.title })
   //   .createParagraphNear()
@@ -196,7 +203,6 @@ const selectImage = (item: IImageItem) => {
 };
 
 defineExpose({ handle: customEditor });
-
 </script>
 
 <style scoped lang="scss">
@@ -301,5 +307,11 @@ button {
   background: rgba(171, 196, 207, 0.745);
   border-radius: 3px;
   margin-bottom: 1rem;
+}
+
+.modal-caption {
+  border: none;
+  margin-top: -15px;
+  font-size: 0.75rem;
 }
 </style>
