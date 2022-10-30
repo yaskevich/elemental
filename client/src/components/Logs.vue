07194a52e5fe43642168b93d493c4aa260244c69
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-10-30 23:30:39 +0300

    render logs list in Logs [client]

--- client/src/components/Logs.vue
+++ client/src/components/Logs.vue
@@ -1,74 +1,126 @@
 <template>
   <!-- <n-page-header subtitle="Review logs" @back="handleBack" style="max-width:600px;margin: 0 auto;padding-left: 1rem;"> -->
-  <n-page-header subtitle="Review logs" style="max-width:600px;margin: 0 auto;padding-left: 1rem;">
-    <n-grid :cols="3">
-      <n-gi>
-User
-      </n-gi>
-      <n-gi>
-Event
-      </n-gi>
-      <n-gi>
-Result      </n-gi>
-
-    </n-grid>
-    <template #title>
-      <a
-        href="https://anyway.fm/"
-        style="text-decoration: none; color: inherit"
-      >
-        Logs
-      </a>
-    </template>
-    <template #header>
-    </template>
-    <template #avatar>
-      <n-avatar
-        src="favicon.ico"
-      />
-    </template>
-    <template #extra>
-      <!-- <n-space>
-        <n-button>Refresh</n-button>
-        <n-dropdown :options="options" placement="bottom-start">
-          <n-button :bordered="false" style="padding: 0 4px">
-            ···
-          </n-button>
-        </n-dropdown>
-      </n-space> -->
-    </template>
-    <template #footer>
-      <!-- As of April 3, 2021 -->
-    </template>
-  </n-page-header>
+  <n-card title="Logs" class="minimal left" :bordered="false" v-if="isLoaded">
+    <n-data-table ref="tableRef" :columns="columns" :data="logs" :row-props="rowProps" />
+  </n-card>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import { useMessage } from 'naive-ui'
+<script setup lang="ts">
+import { ref, reactive, onBeforeMount, h, DefineComponent, toRaw } from 'vue';
+import { RouterLink, useLink } from 'vue-router';
+import { NTime } from 'naive-ui';
+import store from '../store';
+import router from '../router';
 
-export default defineComponent({
-  setup () {
-    const message = useMessage()
-    return {
-      // handleBack () {
-      //   message.info('[onBack]')
-      // },
-      options: [
+const tableRef = ref(null);
+const logs = ref([]);
+const users = ref([]);
+const isLoaded = ref(false);
+const scheme = reactive({} as keyable);
+
+const columns = [
+  // {
+  //   title: 'ID',
+  //   key: 'id',
+  // },
+  {
+    title: 'ID',
+    key: 'record_id',
+    render: (row: IChange) => {
+      return h(
+        RouterLink,
         {
-          label: 'More episodes',
-          key: '1'
+          to: '/' + row.table_name + '/' + row.record_id,
         },
         {
-          label: 'More episodes',
-          key: '2'
-        },
-        {
-          label: 'More episodes',
-          key: '3'
+          default: () => row.record_id,
         }
-      ]
-    }
+      );
+    },
+  },
+  {
+    title: 'Date',
+    key: 'created',
+    render: (row: IChange) => {
+      return h(
+        NTime,
+        {
+          time: new Date(row.created),
+          type: 'relative',
+        },
+        {
+          default: () => new Date(0),
+        }
+      );
+    },
+  },
+  {
+    title: 'User',
+    key: 'user_id',
+    render: (row: IChange) => {
+      const user = users.value[row.user_id] as IUser;
+      return user?.firstname + ' ' + user?.lastname;
+    },
+  },
+  {
+    title: 'Changes',
+    render: (row: IChange) => {
+      return compareRecords(row.data0, row.data1);
+    },
+    ellipsis: {
+      tooltip: true,
+    },
+  },
+];
+
+const rowProps = (row: IChange) => {
+  return {
+    style: 'cursor: pointer;',
+    onClick: () => {
+      // console.log(toRaw(row.data0), toRaw(row.data1));
+      router.push('/logs/' + row.id);
+    },
+  };
+};
+
+const compareRecords = (data0: IComment, data1: IComment) => {
+  const fields = [];
+
+  if (!data0?.title) {
+    return 'CREATED';
   }
-})
+  if (data0.title !== data1.title) {
+    fields.push('Title');
+  }
+  if (data0.priority !== data1.priority) {
+    fields.push('Priority');
+  }
+  if (data0.published !== data1.published) {
+    fields.push('Status');
+  }
+  if (data0.tags.length !== new Set(data0.tags.concat(data1.tags)).size) {
+    fields.push('Tags');
+  }
+  if (JSON.stringify(data0.issues) !== JSON.stringify(data1.issues)) {
+    fields.push('Issues');
+  }
+
+  Object.keys(scheme).forEach(x => {
+    if (JSON.stringify(data0.entry?.[x]) !== JSON.stringify(data1.entry?.[x])) {
+      fields.push(scheme[x].title);
+    }
+  });
+
+  return fields.join(', ');
+};
+
+onBeforeMount(async () => {
+  logs.value = await store.get('logs', String(store?.state?.user?.text_id));
+  const data = await store.get('users');
+  users.value = Object.assign({}, ...data.map((x: any) => ({ [x.id]: x })));
+  // console.log('data from server', logs.value);
+  const texts = await store.get('texts', String(store?.state?.user?.text_id));
+  Object.assign(scheme, Object.assign({}, ...texts.shift().scheme.map((x: any) => ({ [x.id]: x }))));
+  isLoaded.value = true;
+});
 </script>
