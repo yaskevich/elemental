184da322e2b32fe9009c47c92e84a5746003e158
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2022-12-01 23:20:12 +0300

    add function getStats to db [server]

--- server/db.js
+++ server/db.js
@@ -1320,5 +1320,17 @@ export default {
       });
     }
     return data;
+  },
+  async getStats(id) {
+    let data = {};
+    try {
+      const comments = await pool.query('SELECT count(*)::int as total, count(*) FILTER (where published = True) as ready, count(*) FILTER (where published != True) as draft FROM comments WHERE text_id = $1', [id]);
+      const changes = await pool.query("select user_id, count(user_id)::int from logs WHERE (data0->>'text_id' = $1::text or data1->>'text_id' = $1::text) GROUP BY user_id", [id]);
+      const words = await pool.query("select cardinality(comments) as qty, count(*)::int from strings join tokens on strings.token_id = tokens.id  where text_id = $1 and meta='word' group by qty", [id]);
+      data = { comments: comments?.rows, changes: changes?.rows, words: words?.rows };
+    } catch (error) {
+      console.error('Error querying stats', error);
+    }
+    return data;
   }
 };
