a807ae929a6fbced475f72a3950fac7e721446bb
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-02-16 22:04:24 +0300

    render timestamp in PM2 log [server]

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -1,13 +1,15 @@
 module.exports = {
-  apps : [
-      {
-        name: "flowercat",
-        script: "./index.js",
-        watch: false,
-        instance_var: 'INSTANCE_ID',
-        env: {
-            "NODE_ENV": "production"
-        }
+  apps: [
+    {
+      name: 'elemental',
+      script: './index.js',
+      watch: false,
+      time: true,
+      log_date_format: 'YYYY-MM-DD HH:mm',
+      instance_var: 'INSTANCE_ID',
+      env: {
+        NODE_ENV: 'production'
       }
+    }
   ]
-}
+};
