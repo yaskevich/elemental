c1b20e0838cc59787f3cbc6b0e431579aa85388c
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-03-15 13:07:31 +0100

    hide the content if there is nothing done in project in Stats [client]

--- client/src/components/Stats.vue
+++ client/src/components/Stats.vue
@@ -1,94 +1,99 @@
 <template>
   <n-card title="Stats" :bordered="false" class="minimal left">
-    <n-space vertical v-if="isLoaded">
-      <n-tooltip trigger="hover" placement="top">
-        <template #trigger>
-          <n-h1 style="color: orangered; text-align: center; margin-top: -1.5rem; margin-bottom: -0.5rem">{{
-            store.percent(data?.stats?.comments?.ready / data?.stats?.comments?.total)
-          }}</n-h1>
-        </template>
-        {{ data?.stats?.comments?.ready }} published comments to {{ data?.stats?.comments?.draft }} drafts
-      </n-tooltip>
-
-      <div style="text-align: center">
-        <n-tooltip trigger="hover" placement="bottom">
+    <template v-if="isLoaded">
+      <n-space vertical v-if="data?.stats?.comments?.total">
+        <n-tooltip trigger="hover" placement="top">
           <template #trigger>
-            <span>ETC: <n-time :time="data?.stats?.etc" type="date" unix format="MMM yyyy" /></span>
+            <n-h1 style="color: orangered; text-align: center; margin-top: -1.5rem; margin-bottom: -0.5rem">{{
+              store.percent(data?.stats?.comments?.ready / data?.stats?.comments?.total)
+            }}</n-h1>
           </template>
-          Esitmated Time to Complete is <n-time :time="data?.stats?.etc" type="relative" unix />
+          {{ data?.stats?.comments?.ready }} published comments to {{ data?.stats?.comments?.draft }} drafts
         </n-tooltip>
-      </div>
 
-      <n-descriptions label-placement="left" bordered :column="1">
-        <n-descriptions-item>
-          <template #label> Comments </template>
-          <n-tag>{{ data?.stats?.comments?.total }}</n-tag>
-        </n-descriptions-item>
-        <n-descriptions-item>
-          <template #label> Changes </template>
-          <n-tag>{{ data?.stats?.changes?.map((x: any) => x.count).reduce((a: any, b: any) => a + b, 0) }}</n-tag>
-        </n-descriptions-item>
+        <div style="text-align: center">
+          <n-tooltip trigger="hover" placement="bottom">
+            <template #trigger>
+              <span>ETC: <n-time :time="data?.stats?.etc" type="date" unix format="MMM yyyy" /></span>
+            </template>
+            Esitmated Time to Complete is <n-time :time="data?.stats?.etc" type="relative" unix />
+          </n-tooltip>
+        </div>
 
-        <!-- <n-descriptions-item v-for="user in data?.stats?.changes">
+        <n-descriptions label-placement="left" bordered :column="1">
+          <n-descriptions-item>
+            <template #label> Comments </template>
+            <n-tag>{{ data?.stats?.comments?.total }}</n-tag>
+          </n-descriptions-item>
+          <n-descriptions-item>
+            <template #label> Changes </template>
+            <n-tag>{{ data?.stats?.changes?.map((x: any) => x.count).reduce((a: any, b: any) => a + b, 0) }}</n-tag>
+          </n-descriptions-item>
+
+          <!-- <n-descriptions-item v-for="user in data?.stats?.changes">
           <template #label>
             • {{ data?.users[user?.user_id]?.firstname + ' ' + data?.users[user?.user_id]?.lastname }}
           </template>
           {{ user?.count }}
         </n-descriptions-item> -->
 
-        <n-descriptions-item>
-          <template #label> Words </template>
-          <n-tag> {{ total }} </n-tag>
-        </n-descriptions-item>
+          <n-descriptions-item>
+            <template #label> Words </template>
+            <n-tag> {{ total }} </n-tag>
+          </n-descriptions-item>
 
-        <n-descriptions-item v-for="word in data?.stats?.words?.sort((a:any, b:any) => b.count - a.count)">
-          <template #label>
-            • with{{ word?.qty ? ' ' + word?.qty : 'out' }} comment{{ word?.qty === 1 ? '' : 's' }}
-          </template>
-          <n-space>
-            <div style="min-width: 50px">
-              <n-tag>{{ word?.count }}</n-tag>
-            </div>
-            <n-tag type="info"> {{ store.percent(word?.count / total) }} </n-tag>
-          </n-space>
-        </n-descriptions-item>
-      </n-descriptions>
-      <div ref="divRef" style="text-align: center">
-        <Chart
-          :size="{ width: divRef?.clientWidth || 0, height: data?.stats?.changes?.length * 40 }"
-          :data="changes"
-          :margin="margin"
-          direction="vertical"
-          :axis="axis">
-          <template #layers>
-            <Grid strokeDasharray="2,2" />
-            <Bar :dataKeys="['abbr', 'count']" :barStyle="{ fill: '#6f9fde' }" />
-          </template>
-          <template #widgets>
-            <Tooltip
-              borderColor="#48CAE4"
-              :config="{
-                count: { color: 'darkred' },
-                // avg: { color: '#0096c7' },
-                // inc: { color: '#48cae4' },
-              }" />
-          </template>
-        </Chart>
-      </div>
-      <!-- <n-divider></n-divider> -->
-      <div>
-        <n-h4>Tags</n-h4>
-        <n-space vertical>
-          <n-space v-for="tag in data?.stats?.tags" justify="space-between">
-            <n-space v-if="tag?.tags?.length">
-              <n-tag type="success" v-for="tid in tag.tags">{{ data.tags?.[tid][lang] }}</n-tag>
+          <n-descriptions-item v-for="word in data?.stats?.words?.sort((a:any, b:any) => b.count - a.count)">
+            <template #label>
+              • with{{ word?.qty ? ' ' + word?.qty : 'out' }} comment{{ word?.qty === 1 ? '' : 's' }}
+            </template>
+            <n-space>
+              <div style="min-width: 50px">
+                <n-tag>{{ word?.count }}</n-tag>
+              </div>
+              <n-tag type="info"> {{ store.percent(word?.count / total) }} </n-tag>
             </n-space>
-            <n-tag :bordered="false" v-else>without tags</n-tag>
-            <n-tag>{{ tag.qty }}</n-tag>
+          </n-descriptions-item>
+        </n-descriptions>
+        <div ref="divRef" style="text-align: center">
+          <Chart
+            :size="{ width: divRef?.clientWidth || 0, height: data?.stats?.changes?.length * 40 }"
+            :data="changes"
+            :margin="margin"
+            direction="vertical"
+            :axis="axis">
+            <template #layers>
+              <Grid strokeDasharray="2,2" />
+              <Bar :dataKeys="['abbr', 'count']" :barStyle="{ fill: '#6f9fde' }" />
+            </template>
+            <template #widgets>
+              <Tooltip
+                borderColor="#48CAE4"
+                :config="{
+                  count: { color: 'darkred' },
+                  // avg: { color: '#0096c7' },
+                  // inc: { color: '#48cae4' },
+                }" />
+            </template>
+          </Chart>
+        </div>
+        <!-- <n-divider></n-divider> -->
+        <div>
+          <n-h4>Tags</n-h4>
+          <n-space vertical>
+            <n-space v-for="tag in data?.stats?.tags" justify="space-between">
+              <n-space v-if="tag?.tags?.length">
+                <n-tag type="success" v-for="tid in tag.tags">{{ data.tags?.[tid][lang] }}</n-tag>
+              </n-space>
+              <n-tag :bordered="false" v-else>without tags</n-tag>
+              <n-tag>{{ tag.qty }}</n-tag>
+            </n-space>
           </n-space>
-        </n-space>
-      </div>
-    </n-space>
+        </div>
+      </n-space>
+      <n-alert v-else title="No comments" type="warning"
+        >Metrics cannot be counted when there are no comments added</n-alert
+      >
+    </template>
     <!-- <n-divider></n-divider> -->
   </n-card>
 </template>
