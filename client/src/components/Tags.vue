be5c6c677e24b3d46388a3759dc9e09eda51069c
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-03-16 17:47:43 +0100

    render tag title in Tag [client]

--- client/src/components/Tags.vue
+++ client/src/components/Tags.vue
@@ -1,38 +1,35 @@
 <template>
   <n-card title="Tags" :bordered="false" class="minimal left" v-if="isLoaded">
     <template #header-extra>
-      <n-button v-if="!showForm" type="primary" @click="showForm = true;">+ new</n-button>
+      <n-button v-if="!showForm" type="primary" @click="showForm = true">+ new</n-button>
     </template>
 
-    <n-input-group
-      style="display:block;max-width:300px;text-align:center;margin: 0 auto"
-      v-if="showForm"
-    >
+    <n-input-group style="display: block; max-width: 300px; text-align: center; margin: 0 auto" v-if="showForm">
       <n-space vertical>
-        <n-input v-model:value="newTag.en" type="text" placeholder="English title" />
-        <n-input v-model:value="newTag.ru" type="text" placeholder="Russian title" />
+        <n-input v-model:value="newTag.title" type="text" placeholder="Title" />
+        <!-- <n-input v-model:value="newTag.ru" type="text" placeholder="Russian title" /> -->
 
         <n-space justify="center">
-          <n-button tertiary type="warning" @click="showForm = false;">Cancel</n-button>
+          <n-button tertiary type="warning" @click="showForm = false">Cancel</n-button>
           <n-button type="info" @click="editTag(newTag)">Save</n-button>
         </n-space>
       </n-space>
-      <n-divider style="width:300px;text-align: center; margin:auto;padding:1rem;" />
+      <n-divider style="width: 300px; text-align: center; margin: auto; padding: 1rem" />
     </n-input-group>
 
     <div v-if="!tags.length">
       <n-text type="error">There are no tags!</n-text>
     </div>
 
-    <n-space vertical size="large" align="end">
-      <n-grid x-gap="12" cols="3" responsive="screen" v-for="item in tags" :key="item.id">
+    <n-space vertical size="large">
+      <n-grid x-gap="12" cols="2" y-gap="6" responsive="screen" v-for="item in tags" :key="item.id">
         <n-gi>
-          <n-input v-model:value="item.en" placeholder="English title" />
+          <n-input v-model:value="item.title" placeholder="Title" />
         </n-gi>
-        <n-gi>
+        <!-- <n-gi>
           <n-input v-model:value="item.ru" placeholder="English title" />
-        </n-gi>
-        <n-gi style="text-align:right;">
+        </n-gi> -->
+        <n-gi style="text-align: right">
           <n-button type="info" @click="editTag(item)">Save</n-button>
         </n-gi>
       </n-grid>
@@ -41,17 +38,10 @@
 </template>
 
 <script setup lang="ts">
-
 import store from '../store';
 import { ref, reactive, onBeforeMount } from 'vue';
 
-interface ITag {
-  id?: number,
-  en: string,
-  ru: string,
-};
-
-const newTag = reactive({ en: '', ru: '' });
+const newTag = reactive({ title: '' });
 const tags: Array<ITag> = reactive([] as Array<ITag>);
 const showForm = ref(false);
 const isLoaded = ref(false);
@@ -64,26 +54,23 @@ onBeforeMount(async () => {
 });
 
 const editTag = async (tag: ITag) => {
-  console.log('edit tag', tag);
+  // console.log('edit tag', tag);
   const params: ITag = {} as ITag;
   if (tag?.id) {
     params.id = tag.id;
   }
-  params.en = tag.en;
-  params.ru = tag.ru;
+  params.title = tag.title;
   const result = await store.post('tag', params);
   if (result?.data?.id) {
     params.id = result.data.id;
     if (!tag?.id) {
       tags.push(params);
-      newTag.en = newTag.ru = '';
+      newTag.title = '';
     }
   } else {
     console.log('error', result);
   }
 };
-
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
