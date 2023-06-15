07fa77c3dd73c9a3ef0b7aa512f12c4b472bdbf5
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-06-15 21:20:51 +0200

    disallow rewriting of API paths in host [server]

--- server/host.js
+++ server/host.js
@@ -42,6 +42,12 @@ const setup = () => {
   app.set('trust proxy', 1);
   app.use(bodyParser.json({ limit: (options.txtsizelimit || 10) * mb })); // 10 MB
   app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(history({
+    index: '/',
+    rewrites: [
+      { from: /\/api\/.*$/, to: (context) => context.parsedUrl.pathname }
+    ]
+  }));
 
   Object.keys(dirs).forEach((x) => {
     const fullPath = path.join(__dirname, x);
@@ -52,8 +58,6 @@ const setup = () => {
     }
     dirs[x].path = fullPath;
   });
-
-  app.use(history());
 
   return app;
 };
